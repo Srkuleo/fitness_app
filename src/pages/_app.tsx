bf3b2347de4156eff7015516e155d6fe14eac4c9
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-19 22:04:47 +0100

    Added Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import "../styles/global.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
