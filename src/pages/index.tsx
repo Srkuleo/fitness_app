58c0bc4866f2bbbe48053b6ae234f376865f4d71
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-20 13:41:40 +0100

    Added priority to image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,12 @@ const Home: NextPage = () => {
             <SignIn />
           </div>
           <div className="relative w-4/6">
-            <Image src="/images/deadlift.jpg" alt="deadlift" fill />
+            <Image
+              src="/images/deadlift.jpg"
+              alt="deadlift"
+              fill
+              priority={true}
+            />
             <div className="z-10 h-full w-full  bg-gradient-to-l from-slate-900 to-slate-500 opacity-70"></div>
             <div className="absolute bottom-64 right-10 max-w-xl text-right text-2xl text-yellow-50">
               <p>
