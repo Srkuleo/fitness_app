51c4e721034073b6e9c02d36e2182ec9beea6c9b
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-27 03:47:38 +0100

    Removed blur

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        blur: "10px 20px 100px -30px #86efac",
-      },
+      // boxShadow: {
+      //   blur: "10px 20px 160px -40px #86efac",
+      // },
     },
   },
   plugins: [],
