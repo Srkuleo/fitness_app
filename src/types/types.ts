87d2dfbdd8c0e709fd5e7accc7b51d47ef9e4606
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-27 03:47:49 +0100

    New interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,18 @@
 export interface WrapperChild {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+export interface StringProps {
+  text?: string;
+  inputClass?: string;
+  linkClass?: string;
+}
+
+export interface ExternalAuthButtonProps {
+  text: string;
+  jsx: JSX.Element;
+}
+
+export interface LogoProp {
+  svgPlace: JSX.Element;
 }
