3c013d052f8609d43e31ca6fb3f363ae25b4b2a2
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-28 01:20:55 +0100

    New interfaces and variables

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,10 +2,13 @@ export interface WrapperChild {
   children?: React.ReactNode;
 }
 
-export interface StringProps {
-  text?: string;
-  inputClass?: string;
-  linkClass?: string;
+export interface StringProp {
+  text: string;
+}
+
+export interface ClassNameProps {
+  className: string;
+  altClassName?: string;
 }
 
 export interface ExternalAuthButtonProps {
@@ -13,6 +16,21 @@ export interface ExternalAuthButtonProps {
   jsx: JSX.Element;
 }
 
-export interface LogoProp {
-  svgPlace: JSX.Element;
+export interface ExternalAuthProps {
+  google: JSX.Element;
+  twitter: JSX.Element;
+  discord: JSX.Element;
+  googleText: string;
+  twitterTexT: string;
+  discordText: string;
 }
+
+//Variables
+export const inputClass =
+  "placeholder-italic placeholder-gray-600 focus:placeholder-gray-200 rounded-xl p-3 text-sm text-slate-600 outline-green-500 focus:text-base focus:outline-1";
+
+export const linkClassColored = "font-semibold text-green-500 underline";
+
+export const linkClassGrey = "text-sm text-slate-600";
+
+export const signHeaderTextClass = "mt-8 text-lg font-medium text-slate-600";
