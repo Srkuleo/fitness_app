84a81cf8bd8482427038620ac82d2b1b09116fa2
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-28 01:24:49 +0100

    Potentially reusable form

---
+++ src/components/form.tsx
@@ -0,0 +1,38 @@
+// const Form = ({ className }: ClassNameProps) => {
+//     return (
+//       <form action="#" className="flex flex-col ">
+//         <input
+//           type="email"
+//           name="email"
+//           id="email"
+//           placeholder="Email or Username"
+//           required
+//           className={`mb-2 ${className}`}
+//         />
+//         <input
+//           type="password"
+//           name="password"
+//           id="password"
+//           placeholder="Password"
+//           required
+//           className={className}
+//         />
+//         <SignButton text="Sign In" />
+//       </form>
+//     );
+//   };
+
+//   const InputField = () => {
+//     return (
+//         <input
+//         type="email"
+//         name="email"
+//         id="email"
+//         placeholder="Email or Username"
+//         required
+//         className={`mb-2 ${className}`}
+//       />
+//     )
+//   }
+
+export {};
