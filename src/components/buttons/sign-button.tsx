90fa1c067c90c2c780b0f9e6701daedd3025b158
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-29 02:03:46 +0100

    Moved.

--- src/components/buttons/sign-button.tsx
+++
@@ -1,8 +0,0 @@
-import { SignButtonWrapper } from "../wrappers";
-import { type SignButtonProp } from "../../types/types";
-
-const SignButton = ({ buttonText }: SignButtonProp) => {
-  return <SignButtonWrapper>{buttonText}</SignButtonWrapper>;
-};
-
-export default SignButton;
