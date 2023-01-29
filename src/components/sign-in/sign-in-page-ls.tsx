cec64c45f078f954a98e301a498a62627ede69d8
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-29 01:45:28 +0100

    Modified, abstracted form into reusable component

--- src/components/sign-in/sign-in-page-ls.tsx
+++ src/components/sign-in/sign-in-page-ls.tsx
@@ -1,60 +1,27 @@
 import Link from "next/link";
 import NoteSetLogo from "../logos/note-set-logo";
 import { SignFormWrapper } from "../wrappers";
-import SignButton from "../buttons/sign-button";
+import Form from "../form";
 import {
   type ClassNameProps,
-  inputClass,
   linkClassColored,
   linkClassGrey,
-  signHeaderTextClass,
+  type SignProps,
 } from "../../types/types";
 import ExternalAuth from "../external-auth";
-import * as svg from "../svg";
 
-export const SignInForm = () => {
+export const SignInForm = ({ buttons, inputs, buttonText }: SignProps) => {
   return (
     <SignFormWrapper>
       <NoteSetLogo />
-      <SignInHeadingText className={signHeaderTextClass} />
-      <ExternalAuth
-        google={svg.GoogleSvg}
-        twitter={svg.TwitterSvg}
-        discord={svg.DiscordSvg}
-        googleText="Google"
-        twitterTexT="Twitter"
-        discordText="Discord"
-      />
-      <Form className={inputClass} />
+      <SignInHeadingText />
+      <ExternalAuth buttons={buttons} />
+      <Form inputs={inputs} buttonText={buttonText} />
       <LinkQuestions
         className={linkClassColored}
         altClassName={linkClassGrey}
       />
     </SignFormWrapper>
-  );
-};
-
-const Form = ({ className }: ClassNameProps) => {
-  return (
-    <form action="#" className="flex flex-col ">
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Email or Username"
-        required
-        className={`mb-2 ${className}`}
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="Password"
-        required
-        className={className}
-      />
-      <SignButton text="Sign In" />
-    </form>
   );
 };
 
@@ -77,10 +44,12 @@ const LinkQuestions = ({ className, altClassName }: ClassNameProps) => {
   );
 };
 
-const SignInHeadingText = ({ className }: ClassNameProps) => {
+const SignInHeadingText = () => {
   return (
     <>
-      <p className={className}>Sign in and start your workout.</p>
+      <p className="mt-8 text-lg font-medium text-slate-600">
+        Sign in and start your workout.
+      </p>
       <p className="mt-4 text-sm italic text-slate-600">continue with</p>
     </>
   );
