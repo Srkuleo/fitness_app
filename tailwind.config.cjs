acb3f543887ce6add82e421bbd3d37e7a683ba53
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-30 22:09:06 +0100

    Removed custom theme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      // boxShadow: {
-      //   blur: "10px 20px 160px -40px #86efac",
-      // },
-    },
+    extend: {},
   },
   plugins: [],
 };
