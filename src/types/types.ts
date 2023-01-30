be6921b8f8ba9b2479d93b3a9011bcc01960b397
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-30 22:13:19 +0100

    Modified types for better reusability.

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,26 +14,23 @@ export interface ExternalAuthButtonProps extends SignButtonProp {
   buttonSvg: JSX.Element | undefined;
 }
 
-export interface ExternalAuthProps {
-  buttons: ExternalAuthButtonProps[];
-}
-
 export type InputProps = {
   _name: string | undefined;
   placeholder: string | undefined;
   type: string | undefined;
 } & Pick<ClassNameProps, "className">;
 
-export interface FormProps extends SignButtonProp {
+export interface SignProps extends SignButtonProp {
+  buttons: ExternalAuthButtonProps[];
   inputs: InputProps[];
+  checkBox: Pick<InputProps, "_name" | "type">;
 }
-
-export type SignProps = FormProps & ExternalAuthProps;
 
 //Variables
 export const inputClass =
-  "placeholder-italic placeholder-gray-600 focus:placeholder-gray-200 rounded-xl p-3 text-sm text-slate-600 outline-green-500 focus:text-base focus:outline-1";
+  "mb-2 placeholder-italic placeholder-gray-600 focus:placeholder-gray-200 rounded-xl p-3 text-sm text-slate-600 outline-green-500 focus:text-sm focus:outline-1";
 
-export const linkClassColored = "font-semibold text-green-500 underline";
-
-export const linkClassGrey = "text-sm text-slate-600";
+export const greyLinkClass = "text-xs text-slate-600 mb-2";
+export const coloredLinkClass = "font-semibold text-green-500 underline py-1";
+export const externalAuthHeaderClass =
+  "mt-8 text-lg font-medium text-slate-600";
