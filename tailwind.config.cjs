1348eeb3f61e47960ae056e922efd29c535ea018
Srdjan <milicevic.srdjan0@gmail.com>
2023-01-31 23:55:12 +0100

    New custom class for 10px padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        "10px": "10px",
+      },
+    },
   },
   plugins: [],
 };
