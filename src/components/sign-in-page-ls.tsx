28bf2ed2ec30b7ec82897e8bf5f5f76a08ba868b
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-01 22:58:06 +0100

    Less types and props, more concise.

--- src/components/sign-in-page-ls.tsx
+++ src/components/sign-in-page-ls.tsx
@@ -2,47 +2,41 @@ import Link from "next/link";
 import NoteSetLogo from "./logos/note-set-logo";
 import { SignFormWrapper } from "./wrappers";
 import Form from "./reusable/sign/form";
-import {
-  type SignProps,
-  type ClassNameProps,
-  coloredLinkClass,
-  greyLinkClass,
-  externalAuthHeaderClass,
-} from "../types/types";
+import { type SignProps } from "../types/types";
 import ExternalAuth from "./reusable/sign/external-auth";
 
 const SignIn = ({
-  buttons,
+  authButtons,
   inputs,
   checkBox,
-  buttonText,
+  signButton,
 }: Omit<SignProps, "img">) => {
   return (
     <SignFormWrapper>
       <NoteSetLogo />
       <SignInHeadingText />
-      <ExternalAuth buttons={buttons} />
-      <Form inputs={inputs} checkBox={checkBox} buttonText={buttonText} />
-      <LinkQuestions
-        className={coloredLinkClass}
-        altClassName={greyLinkClass}
-      />
+      <ExternalAuth authButtons={authButtons} />
+      <Form inputs={inputs} checkBox={checkBox} signButton={signButton} />
+      <LinkQuestions />
     </SignFormWrapper>
   );
 };
 
-const LinkQuestions = ({ className, altClassName }: ClassNameProps) => {
+const LinkQuestions = () => {
   return (
     <>
-      <p className={altClassName}>
+      <p className="mb-2 text-xs text-slate-600">
         Forgot your{" "}
-        <Link className={className} href="/">
+        <Link className="py-1 font-semibold text-green-500 underline" href="/">
           password?
         </Link>
       </p>
-      <p className={altClassName}>
+      <p className="text-xs text-slate-600">
         New to Noteset?{" "}
-        <Link className={className} href="/sign-up">
+        <Link
+          className="py-1 font-semibold text-green-500 underline"
+          href="/sign-up"
+        >
           Sign up here.
         </Link>
       </p>
@@ -53,7 +47,9 @@ const LinkQuestions = ({ className, altClassName }: ClassNameProps) => {
 const SignInHeadingText = () => {
   return (
     <>
-      <p className={externalAuthHeaderClass}>Sign in and start your workout.</p>
+      <p className="mt-8 text-lg font-medium text-slate-600">
+        Sign in and start your workout.
+      </p>
       <p className="mt-4 text-sm italic text-slate-600">continue with</p>
     </>
   );
