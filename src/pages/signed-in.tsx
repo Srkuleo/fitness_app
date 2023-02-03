110b4cd203be8e0a1d6ce7a719706ab428469072
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-03 23:58:56 +0100

    WIP

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -11,10 +11,18 @@ const SignedIn: NextPage = () => {
         <meta name="description" content="Personalized workout tracker" />
       </Head>
       <main>
-        <p className="mb-20">Welcome to NoteSet, you are signed in!</p>
-        <Link href="/" className="sign-btn">
-          Sign Out
-        </Link>
+        <div className="h-screen w-screen bg-ohp bg-cover bg-no-repeat">
+          <div className="h-screen bg-gradient-to-r from-gray-600/95 to-slate-800/95">
+            <div className="m-auto flex h-screen w-3/5 flex-col items-center">
+              <p className="mb-20 text-xl text-yellow-50">
+                Welcome to NoteSet, you are signed in!
+              </p>
+              <Link href="/" className="sign-btn">
+                Sign Out
+              </Link>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
