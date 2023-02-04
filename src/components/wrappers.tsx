45d64523a6b8391bd42888f09878d4b6a6d47972
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-04 23:46:36 +0100

    Added new wrapper, small changes on left and right

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -6,13 +6,11 @@ export const MainWrapper = ({ children }: WrapperChild) => {
 };
 
 export const LeftWrapper = ({ children }: WrapperChild) => {
-  return (
-    <div className="w-2/6 bg-slate-100 bg-opacity-70 pt-44">{children}</div>
-  );
+  return <div className="w-2/6 bg-slate-100/70 pt-44">{children}</div>;
 };
 
 export const RightWrapper = ({ children }: WrapperChild) => {
-  return <div className="relative w-4/6 overflow-hidden">{children}</div>;
+  return <div className="w-4/6 bg-slate-100/70 p-3">{children}</div>;
 };
 
 export const LogoWrapper = ({ children }: WrapperChild) => {
@@ -32,5 +30,13 @@ export const ExternalAuthButtonWrapper = ({ children }: WrapperChild) => {
     <button className="mt-2 flex w-full flex-row items-center justify-center gap-3 rounded-xl border-2 border-green-300 py-2 text-sm font-semibold text-slate-600 outline-green-700 transition-all ease-in hover:translate-x-1 hover:bg-slate-200/70 focus:outline-1">
       {children}
     </button>
+  );
+};
+
+export const TextWrapper = ({ children }: WrapperChild) => {
+  return (
+    <p className="absolute bottom-52 right-28 w-5/12 text-right text-xl text-yellow-50">
+      {children}
+    </p>
   );
 };
