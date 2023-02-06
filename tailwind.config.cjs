51e590782c0a0b1d0d3834c0a735f2cfc78374c3
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-06 23:42:59 +0100

    Added custom spacing, custom colors next

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    // colors: {
+    //   gray: {
+    //     light: "",
+    //     dark: "",
+    //   },
+    //   green: "",
+    //   textCol: "",
+
+    // },
     extend: {
-      padding: {
-        "10px": "10px",
-      },
       backgroundImage: {
         ohp: "url('/ohp.jpg')",
+        deadlift: "url('/deadlift.jpg')",
+        squat: "url('/squat.jpg')",
+      },
+      spacing: {
+        18: "4.5rem",
+        "2px": "2px",
+        "10px": "10px",
       },
     },
   },
