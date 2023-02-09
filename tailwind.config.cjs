28317a2f88d86133151ae1a3b71c51e0f55b098a
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-09 23:55:33 +0100

    Custom color palette.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,29 +2,51 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    // colors: {
-    //   gray: {
-    //     light: "",
-    //     dark: "",
-    //   },
-    //   green: "",
-    //   textCol: "",
-
-    // },
+    colors: {
+      black: "#000",
+      tp: "transparent",
+      current: "currentColor",
+      slate: {
+        main600: "#475569",
+        light50: "#f8fafc",
+        light100: "#f1f5f9",
+        light200: "#e2e8f0",
+        light300: "#cbd5e1",
+      },
+      green: {
+        main500: "#22c55e",
+        light100: "#dcfce7",
+        light400: "#4ade80",
+        dark600: "#16a34a",
+        dark700: "#15803d",
+      },
+      yellow: {
+        text50: "#fefce8",
+      },
+      sky: {
+        heading500: "#0ea5e9",
+        light300: "#7dd3fc",
+        dark700: "#0369a1",
+      },
+      orange: {
+        button500: "#f97316",
+        button600: "#ea580c",
+      },
+      red: {
+        button500: "#ef4444",
+        button700: "#b91c1c",
+      },
+    },
     extend: {
       backgroundImage: {
         ohp: "url('/ohp.jpg')",
         deadlift: "url('/deadlift.jpg')",
         squat: "url('/squat.jpg')",
-        "gray-sand": "url('/images/gray-sand.jpg')",
-        "gray-lines": "url('/images/gray-lines.jpg')",
+        wavey: "url('/images/wavey.jpg')",
       },
       spacing: {
         5.5: "1.375rem",
         18: "4.5rem",
-        "2px": "2px",
-        "3px": "3px",
-        "10px": "10px",
       },
     },
   },
