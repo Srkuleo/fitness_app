39624cad8258d69dd1c78a62a81d4780554d9d95
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-09 23:57:28 +0100

    Modified.

--- src/components/reusable/sign/external-auth.tsx
+++ src/components/reusable/sign/external-auth.tsx
@@ -21,9 +21,9 @@ const ExternalAuth = ({ authButtons }: Pick<SignProps, "authButtons">) => {
 const Separator = () => {
   return (
     <div className="m-3 flex items-center justify-around">
-      <div className="w-32 border border-green-300"></div>
-      <p className="text-sm italic text-slate-600">or</p>
-      <div className="w-32 border border-green-300"></div>
+      <div className="w-32 border border-green-light400/90"></div>
+      <p className="text-sm italic text-slate-main600">or</p>
+      <div className="w-32 border border-green-light400/90"></div>
     </div>
   );
 };
