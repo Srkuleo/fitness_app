fe482b42fc6644c22652aa6626522e7e42dd70a7
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-09 23:57:21 +0100

    Modified.

--- src/components/logos/note-set-logo.tsx
+++ src/components/logos/note-set-logo.tsx
@@ -11,8 +11,8 @@ const NoteSetLogo = () => {
 };
 
 const NoteSet = (
-  <p className="text-2xl text-slate-600">
-    <span className="font-bold text-green-500">Note</span>Set
+  <p className="text-2xl text-slate-main600">
+    <span className="font-bold text-green-main500">Note</span>Set
   </p>
 );
 
