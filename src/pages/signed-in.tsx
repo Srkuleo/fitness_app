a02e8eb837f067455d8f783b676969a421987ece
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-09 23:58:45 +0100

    New look.

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -9,12 +9,10 @@ import {
   DarkModeIcon,
   SignOutIcon,
   StartIcon,
+  SelectedIcon,
 } from "../components/svg";
 
-/* Povećati ikonice, smanjiti tekst, promeniti boju ikonice na hover da bude siva a ovako zelena, tekst siv a na hover beo
-bg hover zelen, dugme uvek zeleno na hover tamnije 
-U state-u namestiti hover za icon i tekst
-*/
+/* U state-u namestiti hover za icon i tekst*/
 
 const SignedIn: NextPage = () => {
   return (
@@ -25,67 +23,83 @@ const SignedIn: NextPage = () => {
         <meta name="description" content="Personalized workout tracker" />
       </Head>
       <main>
-        <div className="bg-slate-50">
-          <div className="m-1 flex flex-col items-center gap-20 rounded-2xl bg-gray-sand ">
+        <div className="h-screen">
+          <div className="relative flex h-full flex-col items-center gap-20 bg-wavey bg-cover">
             <div className="fixed top-6 right-18 flex flex-col items-end gap-2">
-              <button className="m-0 flex flex-row items-center gap-3 rounded-lg bg-green-600/90 px-3 py-1 font-medium text-yellow-50 transition-all ease-out hover:bg-green-400/80">
+              <button className="m-0 flex items-center gap-3 rounded-lg bg-green-dark700 px-3 py-1 font-medium text-yellow-text50 transition-all ease-out hover:bg-green-light400/60">
                 Options
                 {ArrowDownIcon}
               </button>
-              <div className="flex flex-col rounded-lg bg-slate-50 p-1 text-slate-600">
+              <div className="flex flex-col rounded-lg bg-slate-light50 p-1 text-green-dark700">
                 <Link
-                  className="flex flex-row items-center gap-2 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-green-600 hover:text-yellow-50"
+                  className="flex items-center gap-2 from-green-main500 to-green-dark700 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
                   href="/"
                 >
-                  <span className=" text-green-500 ">{UserIcon}</span>
+                  {UserIcon}
                   Srkuleo
                 </Link>
-                <button className="flex flex-row items-center gap-2 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-green-600 hover:text-yellow-50">
-                  <span className=" text-green-500 ">{EditIcon}</span>
+                <button className="flex items-center gap-2 from-green-main500 to-green-dark700 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50">
+                  {EditIcon}
                   Edit
                 </button>
-                <button className="flex flex-row items-center gap-2 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-green-600 hover:text-yellow-50">
-                  <span className=" text-green-500 ">{LogsIcon}</span>
+                <button className="flex items-center gap-2 from-green-main500 to-green-dark700 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50">
+                  {LogsIcon}
                   View logs
                 </button>
-                <button className="flex flex-row items-center gap-2 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-green-600 hover:text-yellow-50">
-                  <span className=" text-green-500 ">{DarkModeIcon}</span>
+                <button className="flex items-center gap-2 from-green-main500 to-green-dark700 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50">
+                  {DarkModeIcon}
                   Dark mode
                 </button>
-                <div className="my-1 border-b border-green-500/20"></div>
+                <div className="my-1 border-b border-green-main500/20"></div>
                 <Link
-                  className="flex flex-row items-center gap-2 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-green-600 hover:text-yellow-50"
+                  className="flex items-center gap-2 from-green-main500 to-green-dark700 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
                   href="/"
                 >
-                  <span className=" text-green-500 ">{SignOutIcon}</span>
+                  {SignOutIcon}
                   Sign out
                 </Link>
               </div>
             </div>
 
-            <h1 className="mt-28 bg-gradient-to-l from-emerald-400 to-green-400 bg-clip-text text-4xl font-bold uppercase leading-normal text-transparent">
+            <h1 className="mt-20 text-4xl font-bold uppercase text-[#fb923c]">
               Welcome to NoteSet, you are signed in.
             </h1>
             <div className="flex flex-col items-center gap-2">
-              <h3 className="mb-2 text-xl text-yellow-50">
+              <h3 className="mb-2 text-xl text-yellow-text50">
                 Choose your workout for today:
               </h3>
-              <div className="grid grid-cols-3 gap-1 rounded-xl bg-gradient-to-r from-emerald-500/70 to-green-600/80 p-1 text-center text-lg text-yellow-50">
-                <button className="focus:shadow-outline rounded-lg border-2 border-transparent py-1 px-14 font-bold hover:bg-green-400/60 focus:border-green-500 focus:bg-slate-50 focus:text-emerald-700">
-                  Push
+              <div className="flex flex-col gap-2 rounded-xl">
+                <button className="focus:shadow-outline flex items-center gap-10 rounded-lg bg-slate-light100 p-3 text-xl text-slate-main600 focus:bg-green-dark700 focus:text-yellow-text50">
+                  <div className="pr-48 text-left">
+                    <p className="font-semibold">Upper 1</p>
+                    <p className="text-sm italic">workout tooltip</p>
+                  </div>
+                  {SelectedIcon}
                 </button>
-                <button className="focus:shadow-outline rounded-lg border-2 border-transparent py-1 px-14 font-bold hover:bg-green-400/60 focus:border-green-500 focus:bg-slate-50 focus:text-green-600">
-                  Pull
+                <button className="focus:shadow-outline flex items-center gap-10 rounded-lg bg-slate-light100 from-green-dark700 to-sky-dark700 p-3 text-xl text-slate-main600 focus:bg-gradient-to-b focus:text-yellow-text50">
+                  <div className="pr-48 text-left">
+                    <p className="font-semibold">Lower 1</p>
+                    <p className="text-sm italic">workout tooltip</p>
+                  </div>
+                  {SelectedIcon}
                 </button>
-                <button className="focus:shadow-outline rounded-lg border-2 border-transparent py-1 px-14 font-bold hover:bg-green-400/60 focus:border-green-500 focus:bg-slate-50 focus:text-green-600">
-                  Leg
+                <button className="focus:shadow-outline flex items-center gap-10 rounded-lg bg-slate-light100 from-green-dark700 to-sky-dark700 p-3 text-xl text-slate-main600 focus:bg-gradient-to-b focus:text-yellow-text50">
+                  <div className="pr-48 text-left">
+                    <p className="font-semibold">Upper 2</p>
+                    <p className="text-sm italic">workout tooltip</p>
+                  </div>
+                  {SelectedIcon}
                 </button>
-              </div>
-              <div className="rounded-xl bg-slate-50 px-24 py-20 text-lg">
-                Here is tooltip for selected workout.
+                <button className="focus:shadow-outline flex items-center gap-10 rounded-lg bg-slate-light100 from-green-dark700 to-sky-dark700 p-3 text-xl text-slate-main600 focus:bg-gradient-to-b focus:text-yellow-text50">
+                  <div className="pr-48 text-left">
+                    <p className="font-semibold">Lower 2</p>
+                    <p className="text-sm italic">workout tooltip</p>
+                  </div>
+                  {SelectedIcon}
+                </button>
               </div>
             </div>
-            <button className="mb-10 flex flex-row items-center gap-1 rounded-xl bg-gradient-to-r from-orange-500 to-red-500 px-8 py-2 text-lg font-semibold uppercase text-yellow-50 hover:from-orange-600 hover:to-red-700">
+            <button className="mb-20 flex flex-row items-center gap-1 rounded-xl bg-gradient-to-r from-orange-button500 to-red-button500 px-8 py-2 text-lg font-semibold uppercase text-yellow-text50 hover:from-orange-button600 hover:to-red-button700">
               Start
               {StartIcon}
             </button>
