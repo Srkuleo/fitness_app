5a7bc34db610b829af857ecc260e91297e38683a
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-09 23:56:19 +0100

    Modified.

--- src/components/image-loader.tsx
+++ src/components/image-loader.tsx
@@ -20,23 +20,21 @@ export const ImageLoaderDiv = ({ pageImg }: Pick<SignProps, "pageImg">) => {
 
 const ImageFilter = () => {
   return (
-    <div className="h-full w-full rounded-2xl bg-gradient-to-tl from-black via-black/60 to-slate-100/5"></div>
+    <div className="h-full w-full rounded-2xl bg-gradient-to-tl from-black via-black/60 to-tp"></div>
   );
 };
 
 const SignInText = () => {
   return (
     <TextWrapper>
-      <span className="text-3xl font-bold text-green-500">Note</span>
-      <span className="text-3xl text-slate-400">Set</span> is highly
+      <span className="text-3xl font-bold text-green-main500">Note</span>
+      <span className="text-3xl text-slate-light300/80">Set</span> is highly
       personalized, flexible workout tracker and planner, aiming to replace
-      conventional use of a{" "}
-      <span className="line-through decoration-slate-600">notebook</span>
-      .
+      conventional use of a notebook.
       <br />
       Sign up now{" "}
-      <span className="font-bold italic text-green-500">for free</span> to see
-      all the benefits at your hand.
+      <span className="font-bold italic text-green-main500">for free</span> to
+      see all the benefits at your hand.
     </TextWrapper>
   );
 };
@@ -46,11 +44,12 @@ const SignUpText = () => {
     <TextWrapper>
       Listening to your favorite playlist or podcast while writting down every
       session in a gym. Never easier, just install{" "}
-      <span className="text-3xl font-bold text-green-500">Note</span>
-      <span className="text-3xl text-slate-400">Set</span> and get all the
-      benefits of a digital, personalized notebook. <br />
+      <span className="text-3xl font-bold text-green-main500">Note</span>
+      <span className="text-3xl text-slate-light300/80">Set</span> and get all
+      the benefits of a digital, personalized notebook. <br />
       <br />
-      Sign up <span className="font-bold italic text-green-500">for free</span>!
+      Sign up{" "}
+      <span className="font-bold italic text-green-main500">for free</span>!
     </TextWrapper>
   );
 };
