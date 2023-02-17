b6665dfb8191e327e0e5f3cd52fa35aab58fd65b
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-17 03:03:12 +0100

    Changed SelectedIcon.

--- src/components/svg.tsx
+++ src/components/svg.tsx
@@ -316,19 +316,27 @@ export const StartIcon = (
   </svg>
 );
 
-export const SelectedIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="#7dd3fc"
-    viewBox="0 0 24 24"
-    strokeWidth={2}
-    stroke="#0369a1"
-    className="h-8 w-8"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-);
+export const SelectedIcon = ({
+  stroke = "transparent",
+  fill = "transparent",
+}: {
+  stroke?: string;
+  fill?: string;
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={fill}
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke={stroke}
+      className="h-8 w-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
