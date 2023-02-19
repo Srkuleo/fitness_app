509d18a95f8f49f68164db0ddeabdd837b158dab
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-19 01:13:53 +0100

    Modified types and added few global variables.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import * as svg from "../components/svg";
+
 export interface WrapperChild {
   children: React.ReactNode;
 }
@@ -22,17 +24,99 @@ export interface InputProps {
   type: string | undefined;
 }
 
-export interface SignProps {
-  authButtons: ExternalAuthButtonProps[];
-  inputs: InputProps[];
-  checkBox: Pick<InputProps, "_name" | "type">;
-  signButton: SignButtonProps;
-  pageImg: ImageProps;
-}
-
 export type RadioButtonProps = {
   id: number;
   workout: string;
   tooltip: string;
   selected: boolean;
 };
+
+export interface FormProps {
+  inputs: InputProps[];
+  checkBox: Pick<InputProps, "_name" | "type">;
+  signButton: SignButtonProps;
+}
+
+//Variables
+export const authButtons: ExternalAuthButtonProps[] = [
+  { buttonSvg: svg.GoogleSvg, buttonText: "Google" },
+  { buttonSvg: svg.TwitterSvg, buttonText: "Twitter" },
+  { buttonSvg: svg.DiscordSvg, buttonText: "Discord" },
+];
+
+export const signInFormProps: FormProps = {
+  inputs: [
+    { _name: "email", placeholder: "Email", type: "email" },
+    { _name: "password", placeholder: "Password", type: "password" },
+  ],
+  checkBox: {
+    _name: "remember",
+    type: "checkbox",
+  },
+  signButton: {
+    buttonText: "Sign In",
+    href: "/signed-in",
+    onClick: () => console.log("Signed in"),
+  },
+};
+
+export const signUpFormProps: FormProps = {
+  inputs: [
+    {
+      _name: "email",
+      placeholder: "Email",
+      type: "email",
+    },
+    {
+      _name: "username",
+      placeholder: "Username",
+      type: "text",
+    },
+    {
+      _name: "password",
+      placeholder: "Password",
+      type: "password",
+    },
+    {
+      _name: "confPassword",
+      placeholder: "Confirm password",
+      type: "password",
+    },
+  ],
+  checkBox: {
+    _name: "terms and policy",
+    type: "checkbox",
+  },
+  signButton: {
+    buttonText: "Sign Up",
+    href: undefined,
+    onClick: () => console.log("You have signed up."),
+  },
+};
+
+export const initialWorkouts: RadioButtonProps[] = [
+  {
+    id: 1,
+    workout: "UPPER 1",
+    tooltip: "Mix profile day, both chest and back highly activated.",
+    selected: false,
+  },
+  {
+    id: 2,
+    workout: "LOWER 1",
+    tooltip: "Quad heavy day, with few glute/hamstrings exercises.",
+    selected: false,
+  },
+  {
+    id: 3,
+    workout: "UPPER 2",
+    tooltip: "Heavy back day, with complementary chest exercises.",
+    selected: false,
+  },
+  {
+    id: 4,
+    workout: "LOWER 2",
+    tooltip: "All arounder. Leg press + DL, heavy compound day.",
+    selected: false,
+  },
+];
