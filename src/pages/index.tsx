5f8ff54753240a8b2ee7a573227fd95195cfecb0
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-19 01:12:31 +0100

    Removed prop drilling.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { MainWrapper, LeftWrapper, RightWrapper } from "../components/wrappers";
-import { type SignProps } from "../types/types";
-import * as svg from "../components/svg";
 import SignIn from "../components/sign-in-page-ls";
 import { ImageLoaderDiv } from "../components/image-loader";
 
@@ -20,52 +18,15 @@ const Home: NextPage = () => {
       <main>
         <MainWrapper>
           <LeftWrapper>
-            <SignIn
-              authButtons={signInProps.authButtons}
-              inputs={signInProps.inputs}
-              checkBox={signInProps.checkBox}
-              signButton={signInProps.signButton}
-            />
+            <SignIn />
           </LeftWrapper>
           <RightWrapper>
-            <ImageLoaderDiv pageImg={signInProps.pageImg} />
+            <ImageLoaderDiv altTag="deadlift" />
           </RightWrapper>
         </MainWrapper>
       </main>
     </>
   );
-};
-
-const signInProps: SignProps = {
-  authButtons: [
-    { buttonSvg: svg.GoogleSvg, buttonText: "Google" },
-    { buttonSvg: svg.TwitterSvg, buttonText: "Twitter" },
-    { buttonSvg: svg.DiscordSvg, buttonText: "Discord" },
-  ],
-  inputs: [
-    {
-      _name: "email",
-      placeholder: "Email",
-      type: "email",
-    },
-    {
-      _name: "password",
-      placeholder: "Password",
-      type: "password",
-    },
-  ],
-  checkBox: {
-    _name: "remember",
-    type: "checkbox",
-  },
-  signButton: {
-    buttonText: "Sign In",
-    href: "/signed-in",
-    onClick: () => console.log("Signed in"),
-  },
-  pageImg: {
-    altTag: "deadlift",
-  },
 };
 
 export default Home;
