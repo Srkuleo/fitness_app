01118069a2f4e92ec6e0919389717e9ad531d210
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-19 01:11:13 +0100

    Modified to match dark mode.

--- src/components/logos/note-set-logo.tsx
+++ src/components/logos/note-set-logo.tsx
@@ -1,19 +1,29 @@
 import { LogoWrapper } from "../wrappers";
 import { NoteSetLogoSvg } from "../svg";
+import { useAtom } from "jotai";
+import { darkModeAtom } from "../../pages/signed-in";
 
 const NoteSetLogo = () => {
   return (
     <LogoWrapper>
       {NoteSetLogoSvg}
-      {NoteSet}
+      <NoteSet />
     </LogoWrapper>
   );
 };
 
-const NoteSet = (
-  <p className="text-2xl text-slate-main600">
-    <span className="font-bold text-green-main500">Note</span>Set
-  </p>
-);
+const NoteSet = () => {
+  const [isDark] = useAtom(darkModeAtom);
+
+  return (
+    <p
+      className={`text-2xl ${
+        isDark ? "text-slate-light400" : "text-slate-main600"
+      }`}
+    >
+      <span className="font-bold text-green-main500">Note</span>Set
+    </p>
+  );
+};
 
 export default NoteSetLogo;
