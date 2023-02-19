358b6432df60b5cc495d91060320327102181de0
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-19 01:12:43 +0100

    Removed prop drilling.

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { LeftWrapper, RightWrapper, MainWrapper } from "../components/wrappers";
-import { type SignProps } from "../types/types";
-import * as svg from "../components/svg";
 import SignUp from "../components/sign-up-page-ls";
 import { ImageLoaderDiv } from "../components/image-loader";
 
@@ -20,62 +18,15 @@ const SignUpPage: NextPage = () => {
       <main>
         <MainWrapper>
           <LeftWrapper>
-            <SignUp
-              authButtons={signUpProps.authButtons}
-              inputs={signUpProps.inputs}
-              checkBox={signUpProps.checkBox}
-              signButton={signUpProps.signButton}
-            />
+            <SignUp />
           </LeftWrapper>
           <RightWrapper>
-            <ImageLoaderDiv pageImg={signUpProps.pageImg} />
+            <ImageLoaderDiv />
           </RightWrapper>
         </MainWrapper>
       </main>
     </>
   );
-};
-
-const signUpProps: SignProps = {
-  authButtons: [
-    { buttonSvg: svg.GoogleSvg, buttonText: "Sign up with Google" },
-    { buttonSvg: svg.TwitterSvg, buttonText: "Sign up with Twitter" },
-    { buttonSvg: svg.DiscordSvg, buttonText: "Sign up with Discord" },
-  ],
-  inputs: [
-    {
-      _name: "email",
-      placeholder: "Email",
-      type: "email",
-    },
-    {
-      _name: "username",
-      placeholder: "Username",
-      type: "text",
-    },
-    {
-      _name: "password",
-      placeholder: "Password",
-      type: "password",
-    },
-    {
-      _name: "confPassword",
-      placeholder: "Confirm password",
-      type: "password",
-    },
-  ],
-  checkBox: {
-    _name: "agreeToSandPP",
-    type: "checkbox",
-  },
-  signButton: {
-    buttonText: "Sign Up",
-    href: undefined,
-    onClick: () => console.log("You have signed up."),
-  },
-  pageImg: {
-    altTag: "squat",
-  },
 };
 
 export default SignUpPage;
