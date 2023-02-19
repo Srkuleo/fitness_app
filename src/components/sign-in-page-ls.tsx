f7cbc646b6cd9c40816063ff16d587b8d44f6b58
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-19 01:10:25 +0100

    Removed drilling.

--- src/components/sign-in-page-ls.tsx
+++ src/components/sign-in-page-ls.tsx
@@ -2,48 +2,22 @@ import Link from "next/link";
 import NoteSetLogo from "./logos/note-set-logo";
 import { SignFormWrapper } from "./wrappers";
 import Form from "./reusable/sign/form";
-import { type SignProps } from "../types/types";
 import ExternalAuth from "./reusable/sign/external-auth";
+import { authButtons, signInFormProps } from "../types/types";
 
-const SignIn = ({
-  authButtons,
-  inputs,
-  checkBox,
-  signButton,
-}: Omit<SignProps, "pageImg">) => {
+const SignIn = () => {
   return (
     <SignFormWrapper>
       <NoteSetLogo />
       <SignInHeadingText />
       <ExternalAuth authButtons={authButtons} />
-      <Form inputs={inputs} checkBox={checkBox} signButton={signButton} />
+      <Form
+        inputs={signInFormProps.inputs}
+        checkBox={signInFormProps.checkBox}
+        signButton={signInFormProps.signButton}
+      />
       <LinkQuestions />
     </SignFormWrapper>
-  );
-};
-
-const LinkQuestions = () => {
-  return (
-    <>
-      <p className="mb-2 text-xs text-slate-main600">
-        Forgot your{" "}
-        <Link
-          className=" font-semibold text-green-main500 border-b"
-          href="/"
-        >
-          password?
-        </Link>
-      </p>
-      <p className="text-xs text-slate-main600">
-        New to Noteset?{" "}
-        <Link
-          className="font-semibold text-green-main500 border-b"
-          href="/sign-up"
-        >
-          Sign up here.
-        </Link>
-      </p>
-    </>
   );
 };
 
@@ -54,6 +28,28 @@ const SignInHeadingText = () => {
         Sign in and start your workout.
       </p>
       <p className="mt-4 text-sm italic text-slate-main600">continue with</p>
+    </>
+  );
+};
+
+const LinkQuestions = () => {
+  return (
+    <>
+      <p className="mb-2 text-xs text-slate-main600">
+        Forgot your{" "}
+        <Link className=" border-b font-semibold text-green-main500" href="/">
+          password?
+        </Link>
+      </p>
+      <p className="text-xs text-slate-main600">
+        New to Noteset?{" "}
+        <Link
+          className="border-b font-semibold text-green-main500"
+          href="/sign-up"
+        >
+          Sign up here.
+        </Link>
+      </p>
     </>
   );
 };
