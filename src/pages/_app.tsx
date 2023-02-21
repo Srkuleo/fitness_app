d344b2bb57702473d6325cba36485591f3954cdb
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-21 23:51:05 +0100

    Added theme provider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { ThemeProvider } from "next-theme";
 import "../styles/global.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
