ce8a14d74af5e17adcaee3756f5a0082450bac3e
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-21 23:48:43 +0100

    Removed left and right wrapper, refactored others.

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -3,33 +3,31 @@ import { type WrapperChild } from "../types/types";
 //Wrappers for sign-in and sign-up pages
 export const MainWrapper = ({ children }: WrapperChild) => {
   return (
-    <div className="flex h-screen w-screen bg-slate-light50">{children}</div>
-  );
-};
-
-export const LeftWrapper = ({ children }: WrapperChild) => {
-  return <div className="w-2/6 pt-44">{children}</div>;
-};
-
-export const RightWrapper = ({ children }: WrapperChild) => {
-  return <div className="relative m-3 w-4/6">{children}</div>;
-};
-
-export const LogoWrapper = ({ children }: WrapperChild) => {
-  return <div className="flex flex-row items-center gap-1">{children}</div>;
-};
-
-export const SignFormWrapper = ({ children }: WrapperChild) => {
-  return (
-    <div className="mx-auto flex w-1/2 flex-col rounded-2xl text-left">
+    <div className="light-bg dark:dark-bg flex h-screen w-screen">
       {children}
     </div>
   );
 };
 
+export const SignFormWrapper = ({ children }: WrapperChild) => {
+  return (
+    <div className="flex w-2/6 flex-col gap-32 rounded-2xl px-44">
+      {children}
+    </div>
+  );
+};
+
+export const ImageWrapper = ({ children }: WrapperChild) => {
+  return <div className="relative m-3 w-4/6">{children}</div>;
+};
+
+export const LogoWrapper = ({ children }: WrapperChild) => {
+  return <div className="flex items-center gap-1">{children}</div>;
+};
+
 export const ExternalAuthButtonWrapper = ({ children }: WrapperChild) => {
   return (
-    <button className="mt-2 flex flex-row items-center justify-center gap-3 rounded-xl border-2 border-green-light400/90 py-2 text-sm font-semibold text-slate-main600 transition-all ease-in hover:translate-x-1 hover:bg-slate-light200/60">
+    <button className="flex items-center justify-center gap-3 rounded-xl border-2 border-green-light400/90 py-2 text-sm font-semibold text-slate-main600 transition-all ease-in hover:translate-x-1 hover:bg-slate-light200/60 dark:hover:bg-slate-light200">
       {children}
     </button>
   );
