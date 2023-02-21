ee93e1421fd6135c78b8aedce5ee68a8ff75e0f5
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-21 23:50:56 +0100

    Extracted.

---
+++ src/components/reusable/sign/separator.tsx
@@ -0,0 +1,9 @@
+export const Separator = () => {
+  return (
+    <div className="mx-2 my-3 flex items-center gap-1">
+      <div className="w-32 border-b border-green-light400/90"></div>
+      <p className="text-sm italic text-slate-main600">or</p>
+      <div className="w-32 border-b border-green-light400/90"></div>
+    </div>
+  );
+};
