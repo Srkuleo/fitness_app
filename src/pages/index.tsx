de804460194a634971e503bd6b49c2efd35966de
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-22 22:54:56 +0100

    Default export instead of named.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { MainWrapper, SignFormWrapper } from "../components/wrappers";
+import Header from "../components/reusable/sign/header";
 import SignIn from "../components/sign-in-page-ls";
-import { LandingPageImage } from "../components/image-loader";
-import NoteSetLogo from "../components/icons/note-set-logo";
+import LandingPageImage from "../components/image-loader";
 
 const Home: NextPage = () => {
   return (
@@ -26,18 +26,6 @@ const Home: NextPage = () => {
         </MainWrapper>
       </main>
     </>
-  );
-};
-
-export const Header = () => {
-  return (
-    <div className="mt-3 flex">
-      <NoteSetLogo />
-      <div className="flex items-center gap-1 text-2xl text-yellow-text50">
-        <button>GitHub</button>
-        <button>Mode</button>
-      </div>
-    </div>
   );
 };
 
