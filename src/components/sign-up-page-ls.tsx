dadaea8c0fbf26a21d7deba4c57f974f699d3681
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-23 22:26:41 +0100

    Adjusted colors for dark and light mode.

--- src/components/sign-up-page-ls.tsx
+++ src/components/sign-up-page-ls.tsx
@@ -24,16 +24,18 @@ const SignUpHeadingText = () => {
   return (
     <div>
       <p className="text-lg font-semibold leading-6 text-slate-main600 dark:text-yellow-text50">
-        A minimalistic approach to tracking your workout progress
+        A minimalistic approach to tracking your workout progress.
       </p>
-      <p className="mt-4 text-sm italic text-slate-main600">continue with</p>
+      <p className="mt-4 mb-3 text-sm italic text-slate-main600 dark:text-slate-light300">
+        continue with
+      </p>
     </div>
   );
 };
 
 const LinkQuestion = () => {
   return (
-    <p className="text-xs text-slate-main600">
+    <p className="text-xs text-slate-main600 dark:text-slate-light300">
       Already have an acount?{" "}
       <Link href="/" className="border-b font-semibold text-green-main500">
         Sign in here.
