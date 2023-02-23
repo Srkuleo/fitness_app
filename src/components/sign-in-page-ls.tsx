995432cb1b538b46d4c4b6b11e03df3d18a55332
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-23 22:26:32 +0100

    Adjusted colors for dark and light mode.

--- src/components/sign-in-page-ls.tsx
+++ src/components/sign-in-page-ls.tsx
@@ -6,7 +6,7 @@ import { Separator } from "./reusable/sign/separator";
 
 const SignIn = () => {
   return (
-    <div className="flex flex-col">
+    <div>
       <SignInHeadingText />
       <ExternalAuth authButtons={authButtons} />
       <Separator />
@@ -23,10 +23,10 @@ const SignIn = () => {
 const SignInHeadingText = () => {
   return (
     <>
-      <p className="text-lg font-medium text-slate-main600 dark:text-yellow-text50">
+      <p className="text-lg font-semibold text-slate-main600 dark:text-yellow-text50">
         Sign in and start your workout.
       </p>
-      <p className="mt-4 mb-3 text-sm italic text-slate-main600">
+      <p className="mt-4 mb-3 text-sm italic text-slate-main600 dark:text-slate-light300">
         continue with
       </p>
     </>
@@ -36,13 +36,13 @@ const SignInHeadingText = () => {
 const LinkQuestions = () => {
   return (
     <>
-      <p className="mb-2 text-xs text-slate-main600">
+      <p className="mb-2 text-xs text-slate-main600 dark:text-slate-light300">
         Forgot your{" "}
-        <Link className=" border-b font-semibold text-green-main500" href="/">
+        <Link className="border-b font-semibold text-green-main500" href="/">
           password?
         </Link>
       </p>
-      <p className="text-xs text-slate-main600">
+      <p className="text-xs text-slate-main600 dark:text-slate-light300">
         New to Noteset?{" "}
         <Link
           className="border-b font-semibold text-green-main500"
