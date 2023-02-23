0b1f12a314649f617fe98eaa298dacc6f5c26772
Srdjan <milicevic.srdjan0@gmail.com>
2023-02-23 22:29:11 +0100

    Adjusted colors for dark and light mode.

--- src/components/reusable/sign/separator.tsx
+++ src/components/reusable/sign/separator.tsx
@@ -1,9 +1,11 @@
 export const Separator = () => {
   return (
     <div className="mx-2 my-3 flex items-center gap-1">
-      <div className="w-32 border-b border-green-light400/90"></div>
-      <p className="text-sm italic text-slate-main600">or</p>
-      <div className="w-32 border-b border-green-light400/90"></div>
+      <div className="w-32 border-b border-green-main500/70"></div>
+      <p className="text-sm italic text-slate-main600 dark:text-slate-light300">
+        or
+      </p>
+      <div className="w-32 border-b border-green-main500/70"></div>
     </div>
   );
 };
