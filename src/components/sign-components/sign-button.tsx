cbfd92b5e564ed22f305fb203197c7e345ef31e2
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-03 03:57:27 +0100

    Refactored.

---
+++ src/components/sign-components/sign-button.tsx
@@ -0,0 +1,19 @@
+import { type SignButtonProps } from "../../types/types";
+import Link from "next/link";
+
+const SignButton = ({ buttonText, href, onClick }: SignButtonProps) => {
+  if (href) {
+    return (
+      <Link href={href} className="sign-btn">
+        {buttonText}
+      </Link>
+    );
+  }
+  return (
+    <button onClick={onClick} className="sign-btn">
+      {buttonText}
+    </button>
+  );
+};
+
+export default SignButton;
