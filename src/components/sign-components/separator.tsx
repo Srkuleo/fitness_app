48914ef9fa042b645ceed9195a237d62c4bf5bf8
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-03 03:57:19 +0100

    Refactored.

---
+++ src/components/sign-components/separator.tsx
@@ -0,0 +1,11 @@
+const Separator = () => {
+  return (
+    <div className="mx-2 my-3 flex items-center gap-1">
+      <div className="w-32 border-b border-green-main500/70"></div>
+      <p className="italic text-slate-main600 dark:text-slate-light300">or</p>
+      <div className="w-32 border-b border-green-main500/70"></div>
+    </div>
+  );
+};
+
+export default Separator;
