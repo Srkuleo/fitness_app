efbe4c31d4bd084bfea99566a5d9e3badf7c11c5
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-03 03:58:20 +0100

    Refactored.

---
+++ src/components/svg-components/note-set-logo.tsx
@@ -0,0 +1,21 @@
+import { LogoWrapper } from "../wrappers";
+import { NoteSetLogoSvg } from "./svg";
+
+const NoteSetLogo = () => {
+  return (
+    <LogoWrapper>
+      {NoteSetLogoSvg}
+      <NoteSetText />
+    </LogoWrapper>
+  );
+};
+
+export const NoteSetText = () => {
+  return (
+    <span className="text-2xl font-medium text-slate-light500 dark:text-slate-light400">
+      <span className="font-bold text-green-main500">Note</span>Set
+    </span>
+  );
+};
+
+export default NoteSetLogo;
