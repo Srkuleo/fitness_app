a41d1f31ff9466ac5d45ee1ee52189fd523cd723
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-03 02:28:55 +0100

    Refactored.

--- src/components/radio-buttons-content.tsx
+++ src/components/radio-buttons-content.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
 import { initialWorkouts } from "../types/types";
-import { StartIcon, SelectedIcon } from "./icons/svg";
+import { StartIcon, SelectedIcon } from "./svg-components/svg";
 import { useState } from "react";
-import { useTheme } from "next-themes";
 
 const RadioButtonsContent = () => {
   const [workouts, setWorkouts] = useState(initialWorkouts);
-  const { theme } = useTheme();
 
   function toggleIsSelected(id: number) {
     const newWorkouts = workouts.map((workout) => {
@@ -26,33 +23,14 @@ const RadioButtonsContent = () => {
   }
 
   return (
-    <div className="relative flex h-screen flex-col items-center">
-      {theme === "dark" ? (
-        <Image
-          src="/SVG/dark.svg"
-          alt="dark pattern"
-          fill={true}
-          priority={true}
-          style={{ objectFit: "cover" }}
-          className="relative z-0"
-        />
-      ) : (
-        <Image
-          src="/SVG/light.svg"
-          alt="pattern"
-          fill={true}
-          priority={true}
-          style={{ objectFit: "cover" }}
-          className="relative z-0"
-        />
-      )}
-      <h1 className="relative z-10 mt-36 mb-24 bg-gradient-to-r from-orange-button500 to-red-button500 bg-clip-text text-5xl font-bold text-tp">
+    <div className="relative z-10 mx-auto flex flex-col items-center">
+      <h1 className="mt-36 mb-24 text-5xl font-bold text-orange-button500">
         Welcome, Srkuleo.
       </h1>
-      <h3 className="relative z-10 mb-6 text-xl font-semibold text-slate-main600 dark:text-yellow-text50">
+      <h3 className="mb-6 text-2xl font-semibold text-slate-main600 dark:text-yellow-text50">
         Choose your workout for today:
       </h3>
-      <div className="relative z-10 flex flex-col gap-2 rounded-xl">
+      <div className="flex flex-col gap-2 rounded-xl">
         {workouts.map((workout) => {
           return (
             <button
@@ -62,7 +40,7 @@ const RadioButtonsContent = () => {
               key={workout.id}
               className={`${
                 workout.selected
-                  ? "bg-gradient-to-r from-sky-dark800 to-sky-dark700 text-yellow-text50"
+                  ? "bg-gradient-to-r from-green-dark600/90 to-green-dark700 text-yellow-text50"
                   : "bg-slate-light50"
               } rounded-lg px-6 py-4`}
             >
@@ -93,7 +71,7 @@ const RadioButtonsContent = () => {
           );
         })}
       </div>
-      <button className="relative z-10 mt-12 flex items-center gap-1 rounded-xl bg-gradient-to-r from-orange-button500 to-red-button500 px-8 py-2 text-lg font-semibold uppercase text-yellow-text50 hover:from-orange-button600 hover:to-red-button700">
+      <button className="mt-12 flex items-center gap-1 rounded-xl bg-gradient-to-r from-orange-button500 via-orange-button500 to-red-button500 px-8 py-2 text-lg font-semibold uppercase text-yellow-text50 hover:from-orange-button600 hover:to-red-button700">
         Start
         {StartIcon}
       </button>
