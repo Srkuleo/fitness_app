bcfe28685346400a672b32b14e16db60f91c56eb
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-04 11:55:45 +0100

    Small change in text.

--- src/components/sign-components/heading-text.tsx
+++ src/components/sign-components/heading-text.tsx
@@ -4,7 +4,7 @@ const HeadingText = ({ page }: { page: string }) => {
       <p className="text-xl font-bold text-slate-main600 dark:text-yellow-text50">
         {page === "sign in"
           ? "Sign in and start your workout."
-          : "A minimalistic approach to tra-cking your workout progress."}
+          : "A minimalistic approach to tracking your workout progress."}
       </p>
       <p className="mt-4 mb-3 italic text-slate-main600 dark:text-slate-light300">
         continue with
