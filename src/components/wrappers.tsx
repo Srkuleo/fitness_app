7ce5b0cbea443c7fbccaa0a1341b581ac9ceac2d
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-04 11:55:12 +0100

    Easier to read className added w-full

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -13,7 +13,14 @@ export const LogoWrapper = ({ children }: WrapperChild) => {
 
 export const ExternalAuthButtonWrapper = ({ children }: WrapperChild) => {
   return (
-    <button className="flex items-center justify-center gap-3 rounded-xl border-2 border-green-main500 bg-slate-light50 py-2 text-sm font-semibold text-slate-main600 transition-all ease-in hover:translate-x-1 hover:bg-slate-light200/90 dark:bg-slate-dark800 dark:text-yellow-text50 dark:hover:bg-slate-light500/50">
+    <button
+      className="flex w-full items-center justify-center gap-3 
+      rounded-xl border-2 border-green-main500 bg-slate-light50 
+      py-2 text-sm font-semibold text-slate-main600 
+      transition-all ease-in hover:translate-x-1 
+    hover:bg-slate-light200/90 dark:bg-slate-dark800 
+    dark:text-yellow-text50 dark:hover:bg-slate-light500/50"
+    >
       {children}
     </button>
   );
