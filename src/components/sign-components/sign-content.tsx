c561ef4cb91c5ddb9b98e2e6c0c7e47d3583f470
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-06 23:54:57 +0100

    .

--- src/components/sign-components/sign-content.tsx
+++ src/components/sign-components/sign-content.tsx
@@ -1,4 +1,4 @@
-import HeadingText from "./heading-text";
+import { SignContentHeading } from "../headings";
 import ExternalAuth from "./external-auth";
 import Separator from "./separator";
 import Form from "./form";
@@ -7,7 +7,7 @@ import LinkQuestions from "./link-questions";
 const SignContent = ({ page }: { page: string }) => {
   return (
     <div className="mx-auto w-[305px]">
-      <HeadingText page={page} />
+      <SignContentHeading page={page} />
       <ExternalAuth />
       <Separator />
       <Form page={page} />
