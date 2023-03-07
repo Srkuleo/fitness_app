03d8778e71931ea67676554adedd048997c08edb
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-07 01:41:30 +0100

    .

--- src/components/signed-in-components/radio-buttons-content.tsx
+++ src/components/signed-in-components/radio-buttons-content.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { type RadioButtonProps } from "../../types/types";
+import { type RadioButtonProps, type EditingProps } from "../../types/types";
 import { RadioButtonHeading } from "../headings";
 import RadioButtons from "./radiobuttons";
 import { StartButton } from "../buttons";
@@ -8,19 +8,16 @@ const initialWorkout: RadioButtonProps = {
   id: 0,
   name: "",
   tooltip: "",
-  selected: false,
+  selected: false
 };
 
 const RadioButtonsContent = ({
   isAdding,
   toggleAdding,
-}: {
-  isAdding: boolean;
-  toggleAdding: () => void;
-}) => {
+  closeDropDown
+}: EditingProps & Record<"closeDropDown", () => void>) => {
   const [workouts, setWorkouts] = useState<RadioButtonProps[]>([]);
-  const [newWorkout, setNewWorkout] =
-    useState<RadioButtonProps>(initialWorkout);
+  const [newWorkout, setNewWorkout] = useState<RadioButtonProps>(initialWorkout);
   const [currentId, setCurrentId] = useState(0);
 
   function toggleIsSelected(id: number) {
@@ -29,12 +26,12 @@ const RadioButtonsContent = ({
         if (workout.id === id) {
           return {
             ...workout,
-            selected: true,
+            selected: true
           };
         } else {
           return {
             ...workout,
-            selected: false,
+            selected: false
           };
         }
       });
@@ -56,62 +53,44 @@ const RadioButtonsContent = ({
   }
 
   return (
-    <div className="relative z-0 mx-auto flex flex-col items-center">
-      <RadioButtonHeading />
-      {workouts.length === 0 ? (
-        <div className="space-y-2 rounded-lg bg-slate-light50 py-32 px-16 text-center text-lg font-semibold text-slate-main600">
-          <p>You don`t have any existing workout available.</p>
-          <button
-            className="rounded-lg bg-slate-main600 px-8 py-2 text-base text-slate-light50 hover:bg-slate-main600/80"
-            onClick={toggleAdding}
-          >
-            Add a workout
-          </button>
-        </div>
-      ) : (
-        <>
-          <RadioButtons
-            workouts={workouts}
-            toggleIsSelected={toggleIsSelected}
-          />
-          <StartButton />
-        </>
-      )}
+    <>
       {isAdding && (
         <div
-          role="button"
+          role='button'
           onClick={toggleAdding}
-          className="fixed top-0 z-20 h-screen w-screen cursor-default bg-black/70"
+          onMouseEnter={closeDropDown}
+          className='fixed top-0 z-20 h-screen w-screen cursor-default bg-black/80'
         >
-          <div className="center absolute top-[50%] left-[50%] m-auto flex flex-col items-center gap-10 rounded-lg bg-slate-light50 p-20">
+          <div className='center absolute top-[50%] left-[50%] flex flex-col gap-10 rounded-lg bg-slate-light50 p-20'>
+            <p className='text-lg font-semibold text-slate-main600'>
+              Please fill all fields before pressing {`"Add"`} button.
+            </p>
             <form
               onSubmit={handleSubmit}
-              className="space-x-10"
-              onClick={(e: React.MouseEvent<HTMLFormElement>) =>
-                e.stopPropagation()
-              }
+              className='space-x-10'
+              onClick={(e: React.MouseEvent<HTMLFormElement>) => e.stopPropagation()}
             >
               <input
                 required
-                className="input-field"
-                type="text"
-                placeholder="Workout"
-                name="name"
+                className='input-field'
+                type='text'
+                placeholder='Workout'
+                name='name'
                 value={newWorkout.name}
                 onChange={handleValue}
               />
               <input
                 required
-                className="input-field"
-                type="text"
-                placeholder="Tooltip"
-                name="tooltip"
+                className='input-field'
+                type='text'
+                placeholder='Tooltip'
+                name='tooltip'
                 value={newWorkout.tooltip}
                 onChange={handleValue}
               />
               <button
-                type="submit"
-                className="rounded-lg bg-slate-main600 px-8 py-2 text-slate-light50 hover:bg-slate-main600/80"
+                type='submit'
+                className='rounded-lg bg-slate-main600 px-8 py-2 text-slate-light50 hover:bg-slate-main600/80'
               >
                 Add
               </button>
@@ -119,7 +98,26 @@ const RadioButtonsContent = ({
           </div>
         </div>
       )}
-    </div>
+      <div className='relative z-0 mx-auto flex flex-col items-center'>
+        <RadioButtonHeading />
+        {workouts.length === 0 ? (
+          <div className='space-y-2 rounded-lg bg-slate-light50 py-32 px-16 text-center text-lg font-semibold text-slate-main600'>
+            <p>You don`t have any existing workout available.</p>
+            <button
+              className='rounded-lg bg-slate-main600 px-8 py-2 text-base text-slate-light50 hover:bg-slate-main600/80'
+              onClick={toggleAdding}
+            >
+              Add a workout
+            </button>
+          </div>
+        ) : (
+          <>
+            <RadioButtons workouts={workouts} toggleIsSelected={toggleIsSelected} />
+            <StartButton />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
