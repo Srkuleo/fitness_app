f289b65b6a7e919a8a7bebe076ed6e88f8897fd5
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-07 01:56:01 +0100

    Reverted.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,4 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  printWidth: 90,
-  proseWrap: "always",
-  jsxSingleQuote: true,
-  semi: true,
-  trailingComma: "none",
-  tabWidth: 2
 };
