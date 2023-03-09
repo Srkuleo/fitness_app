7ceea81b4f6fec9d521fd78b2213f23857396899
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-09 03:24:22 +0100

    .

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,29 +2,27 @@ export interface WrapperChild {
   children: React.ReactNode;
 }
 
-export interface SignButtonProps {
-  buttonText: string | undefined;
-  href: string | undefined;
-  onClick: () => void | undefined;
+export interface ModeButtonProps {
+  size: number;
+  theme: string;
+  toggleMode: () => void;
 }
 
 export interface ExternalAuthButtonProps
   extends Pick<SignButtonProps, "buttonText"> {
-  buttonSvg: JSX.Element | undefined;
+  buttonSvg: JSX.Element;
 }
 
 export interface InputProps {
-  _name: string | undefined;
-  placeholder: string | undefined;
-  type: string | undefined;
+  _name: string;
+  placeholder: string;
+  type: string;
 }
 
-export type RadioButtonProps = {
-  id: number;
-  name: string;
-  tooltip: string;
-  selected: boolean;
-};
+export interface SignButtonProps {
+  buttonText: string;
+  href: string;
+}
 
 export interface FormProps {
   form: {
@@ -35,75 +33,21 @@ export interface FormProps {
   signButton: SignButtonProps;
 }
 
-export interface ModeButtonProps {
-  size: number;
-  theme: string | undefined;
-  toggleMode: () => void;
-}
-
 export interface OptionMenuProps {
   isOpen: boolean;
   toggleDropDown: () => void;
+  toggleEdit: () => void;
 }
 
-export interface EditingProps {
-  isAdding: boolean;
-  toggleAdding: () => void;
+export interface RadioButtonProps {
+  id: number;
+  name: string;
+  tooltip: string;
 }
 
-//Variables
-export const signIn: FormProps = {
-  form: {
-    id: "sign-in",
-    inputs: [
-      { _name: "email", placeholder: "Email", type: "email" },
-      { _name: "password", placeholder: "Password", type: "password" },
-    ],
-  },
-  checkBox: {
-    _name: "remember",
-    type: "checkbox",
-  },
-  signButton: {
-    buttonText: "Sign In",
-    href: "/signed-in",
-    onClick: () => console.log("Signed in"),
-  },
-};
+type DispatchTuple = "added" | "edited" | "deleted";
 
-export const signUp: FormProps = {
-  form: {
-    id: "sign-up",
-    inputs: [
-      {
-        _name: "email",
-        placeholder: "Email",
-        type: "email",
-      },
-      {
-        _name: "username",
-        placeholder: "Username",
-        type: "text",
-      },
-      {
-        _name: "password",
-        placeholder: "Password",
-        type: "password",
-      },
-      {
-        _name: "confPassword",
-        placeholder: "Confirm password",
-        type: "password",
-      },
-    ],
-  },
-  checkBox: {
-    _name: "terms and policy",
-    type: "checkbox",
-  },
-  signButton: {
-    buttonText: "Sign Up",
-    href: undefined,
-    onClick: () => console.log("You have signed up."),
-  },
-};
+export interface WorkoutsAction {
+  type: DispatchTuple;
+  workout: RadioButtonProps;
+}
