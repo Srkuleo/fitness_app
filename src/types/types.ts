e727158b1fc30f7b99885bc2558f38ed3ae95c75
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-10 12:13:49 +0100

    New types.

--- src/types/types.ts
+++ src/types/types.ts
@@ -37,17 +37,59 @@ export interface OptionMenuProps {
   isOpen: boolean;
   toggleDropDown: () => void;
   toggleEdit: () => void;
+  removeSelectedId: () => void;
 }
 
-export interface RadioButtonProps {
+export interface WorkoutProps {
   id: number;
   name: string;
   tooltip: string;
+}
+export interface RBComponentProps {
+  workouts: WorkoutProps[];
+  isEditing: boolean;
+  toggleEdit: () => void;
+  handleAddWorkout: ({
+    name,
+    tooltip,
+  }: Pick<WorkoutProps, "name" | "tooltip">) => void;
+  handleChangeWorkout: (workout: WorkoutProps) => void;
+  handleDeleteWorkout: (workout: WorkoutProps) => void;
+  selectedId: number | undefined;
+  handleId: (id: number) => void;
 }
 
 type DispatchTuple = "added" | "edited" | "deleted";
 
 export interface WorkoutsAction {
   type: DispatchTuple;
-  workout: RadioButtonProps;
+  workout: WorkoutProps;
+}
+
+export type RadioButtonProps = WorkoutProps & {
+  selectedId: number | undefined;
+  handleId: (id: number) => void;
+};
+
+export interface AddingFormProps {
+  toggleAdding: () => void;
+  tempWorkout: WorkoutProps;
+  modifyTempWorkout: (tempWorkout: WorkoutProps) => void;
+  handleAddWorkout: (workout: WorkoutProps) => void;
+  handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface ChangingFormProps {
+  toggleChanging: () => void;
+  tempWorkout: WorkoutProps;
+  modifyTempWorkout: (workout: WorkoutProps) => void;
+  handleChangeWorkout: (workout: WorkoutProps) => void;
+  handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface EditingButtonsProps {
+  workout: WorkoutProps;
+  modifyTempWorkout: (workout: WorkoutProps) => void;
+  toggleChanging: () => void;
+  handleDeleteWorkout: (workout: WorkoutProps) => void;
 }
