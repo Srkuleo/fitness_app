a603e4b8a4fc66aacec835f55c53d40ad5523a70
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-15 00:00:21 +0100

    .

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -1,73 +1,30 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Pattern } from "../components/svg-components/circle-pattern";
-import { Curve } from "../components/svg-components/curve-pattern";
-import NavBar from "../components/signed-in-components/nav-bar";
+import { useWorkouts } from "../hooks/useWorkouts";
+import { useEdit } from "../hooks/useEdit";
+import { useHandleId } from "../hooks/useHandleId";
+import type { WorkoutProps } from "../types/types";
+import BackgroundPattern from "../components/svg-components/background-pattern";
+import {
+  FixedLogo,
+  OptionsMenu,
+} from "../components/signed-in-components/fixed-components";
 import { RadioButtonContentWrapper } from "../components/wrappers";
 import { RadioButtonHeading } from "../components/headings";
 import RBComponent from "../components/signed-in-components/radiobuttons";
 import { StartButton } from "../components/buttons";
-import { useReducer, useState } from "react";
-import { workoutsReducer } from "../functions and variables/functions";
-import type { WorkoutProps } from "../types/types";
-
-let nextId = 0;
 
 const initialWorkouts: WorkoutProps[] = [
-  { id: nextId++, name: "Upper 1", tooltip: "Good one, could be better." },
-  { id: nextId++, name: "Upper 2", tooltip: "A little bit better, imo." },
-  { id: nextId++, name: "Lower 1", tooltip: "The best one so far!" },
+  // { id: 1, name: "Upper 1", tooltip: "Good one, could be better." },
+  // { id: 2, name: "Upper 2", tooltip: "A little bit better, imo." },
+  // { id: 3, name: "Lower 1", tooltip: "The best one so far!" },
 ];
 
 const SignedIn: NextPage = () => {
-  const [workouts, dispatch] = useReducer(workoutsReducer, initialWorkouts);
-  const [isOpen, setIsOpen] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [selectedId, setSelectedId] = useState<number | undefined>(undefined);
-
-  function handleAddWorkout({
-    name,
-    tooltip,
-  }: Pick<WorkoutProps, "name" | "tooltip">) {
-    dispatch({
-      type: "added",
-      workout: {
-        id: nextId++,
-        name: name,
-        tooltip: tooltip,
-      },
-    });
-  }
-
-  function handleChangeWorkout(workout: WorkoutProps) {
-    dispatch({
-      type: "edited",
-      workout: workout,
-    });
-  }
-
-  function handleDeleteWorkout(workout: WorkoutProps) {
-    dispatch({
-      type: "deleted",
-      workout: workout,
-    });
-  }
-
-  function toggleDropDown() {
-    setIsOpen(!isOpen);
-  }
-
-  function toggleEdit() {
-    setIsEditing(!isEditing);
-  }
-
-  function handleId(id: number) {
-    setSelectedId(id);
-  }
-
-  function removeSelectedID() {
-    setSelectedId(undefined);
-  }
+  const { workouts, addWorkout, changeWorkout, removeWorkout } =
+    useWorkouts(initialWorkouts);
+  const { isEditing, toggleEdit } = useEdit();
+  const { selectedId, addSelectedId, removeSelectedId } = useHandleId();
 
   return (
     <>
@@ -77,27 +34,25 @@ const SignedIn: NextPage = () => {
         <meta name="description" content="Personalized workout tracker" />
       </Head>
       <main>
-        <Pattern />
-        <Curve />
-        <NavBar
-          isOpen={isOpen}
-          toggleDropDown={toggleDropDown}
+        <BackgroundPattern />
+        <FixedLogo />
+        <OptionsMenu
           toggleEdit={toggleEdit}
-          removeSelectedId={removeSelectedID}
+          removeSelectedId={removeSelectedId}
         />
         <RadioButtonContentWrapper>
           <RadioButtonHeading />
           <RBComponent
             workouts={workouts}
+            addWorkout={addWorkout}
+            changeWorkout={changeWorkout}
+            removeWorkout={removeWorkout}
             isEditing={isEditing}
             toggleEdit={toggleEdit}
-            handleAddWorkout={handleAddWorkout}
-            handleChangeWorkout={handleChangeWorkout}
-            handleDeleteWorkout={handleDeleteWorkout}
             selectedId={selectedId}
-            handleId={handleId}
+            addSelectedId={addSelectedId}
           />
-          <StartButton />
+          <StartButton workouts={workouts} />
         </RadioButtonContentWrapper>
       </main>
     </>
