e668a52262b09e8ae87c810766160d236f0d9d59
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-14 23:58:49 +0100

    .

---
+++ src/components/svg-components/background-pattern.tsx
@@ -0,0 +1,13 @@
+import { Pattern } from "./circle-pattern";
+import { Curve } from "./curve-pattern";
+
+const BackgroundPattern = () => {
+  return (
+    <>
+      <Pattern />
+      <Curve />
+    </>
+  );
+};
+
+export default BackgroundPattern;
