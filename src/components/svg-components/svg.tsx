9dd5de5239cba64daeffc2ce8f130d574e3e8ee3
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-23 14:06:48 +0100

    Removed selected icon, rendering changed.

--- src/components/svg-components/svg.tsx
+++ src/components/svg-components/svg.tsx
@@ -328,30 +328,6 @@ export const StartIcon = (
   </svg>
 );
 
-export const SelectedIcon = ({
-  stroke = "transparent"
-}: {
-  stroke?: string;
-}) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="transparent"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke={stroke}
-      width={32}
-      height={32}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
-};
-
 export const DeleteIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
