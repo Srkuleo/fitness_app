d5d2d45f6796b8354ef6e60317ae405d453034ba
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-24 20:34:09 +0100

    .

--- src/components/signed-in-components/cards-navigation.tsx
+++ src/components/signed-in-components/cards-navigation.tsx
@@ -3,21 +3,21 @@ import { PrevIcon, NextIcon } from "../svg-components/svg";
 
 const CardsNavigation = ({
   workouts,
-  handlePrevIndex,
-  handleNextIndex,
+  prevCard,
+  nextCard,
 }: CardsNavigationProps) => {
   return (
-    <div className="absolute inset-0 flex items-center justify-between p-1">
+    <div className="absolute inset-0 flex items-center justify-between">
       <button
-        className="relative z-10 p-2 text-slate-main600 transition-all ease-out hover:-translate-x-1 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
-        onClick={handlePrevIndex}
+        className="relative p-1 text-slate-main600 transition-all ease-out hover:-translate-x-1 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
+        onClick={prevCard}
         disabled={workouts.length < 2}
       >
         {PrevIcon}
       </button>
       <button
-        className="relative z-10 p-2 text-slate-main600 transition-all ease-out hover:translate-x-1 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
-        onClick={handleNextIndex}
+        className="relative p-1 text-slate-main600 transition-all ease-out hover:translate-x-1 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
+        onClick={nextCard}
         disabled={workouts.length < 2}
       >
         {NextIcon}
