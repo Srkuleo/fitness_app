9c444f8453030274a9fc77d70c94a052655ba5e5
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-24 19:35:00 +0100

    .

--- src/components/signed-in-components/edit-bar.tsx
+++ src/components/signed-in-components/edit-bar.tsx
@@ -1,8 +1,8 @@
 import type { EditBarProps } from "../../types/types";
 import { AddIcon, DeleteIcon, DoneIcon, EditIcon } from "../svg-components/svg";
 
-export const EditBar = ({
-  workout,
+const EditBar = ({
+  currWorkout,
   modifyTempWorkout,
   changingState,
   removeWorkout,
@@ -11,21 +11,21 @@ export const EditBar = ({
   toggleEdit,
 }: EditBarProps) => {
   return (
-    <div className="mx-18 flex justify-between">
-      <div className="flex items-center space-x-2">
+    <div className="flex justify-between">
+      <div className="space-x-2">
         <button
-          className="rounded-full bg-green-light300 p-smallButton text-slate-main600 transition-all duration-200 ease-in hover:translate-y-1"
+          className="rounded-full bg-green-light300 p-2 text-slate-main600 transition-all duration-200 ease-in hover:translate-y-1"
           onClick={() => {
-            modifyTempWorkout(workout);
+            modifyTempWorkout(currWorkout);
             changingState();
           }}
         >
           {EditIcon}
         </button>
         <button
-          className="rounded-full bg-red-button400 p-smallButton text-slate-main600 transition-all duration-200 ease-in hover:translate-y-1"
+          className="rounded-full bg-red-button400 p-2 text-slate-main600 transition-all duration-200 ease-in hover:translate-y-1"
           onClick={() => {
-            removeWorkout(workout.id);
+            removeWorkout(currWorkout.id);
             switchOnRemove();
           }}
         >
@@ -34,13 +34,13 @@ export const EditBar = ({
       </div>
       <div className="space-x-2">
         <button
-          className="rounded-full bg-slate-light400 p-mediumButton text-slate-light50 transition-all ease-out hover:bg-slate-main600"
+          className="rounded-full bg-slate-light400 p-2 text-slate-light50 transition-all ease-out hover:bg-slate-main600"
           onClick={addingState}
         >
           {AddIcon}
         </button>
         <button
-          className="rounded-full bg-slate-light400 p-mediumButton text-slate-light50 transition-all ease-out hover:bg-slate-main600"
+          className="rounded-full bg-slate-light400 p-2 text-slate-light50 transition-all ease-out hover:bg-slate-main600"
           onClick={toggleEdit}
         >
           {DoneIcon}
@@ -49,3 +49,5 @@ export const EditBar = ({
     </div>
   );
 };
+
+export default EditBar;
