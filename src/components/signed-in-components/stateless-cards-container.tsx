d144a3d5cf7511dabef6f77cd6445b3aacf1cf54
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-25 18:52:21 +0100

    Component that renders if workouts not available.

---
+++ src/components/signed-in-components/stateless-cards-container.tsx
@@ -0,0 +1,37 @@
+import type { StatelessCardsContainerProps } from "../../types/types";
+import { DbIcon } from "../svg-components/svg";
+import { NewWorkoutBtn } from "../buttons";
+import AddingForm from "./adding-form";
+
+const StatelessCardsContainer = ({
+  addingState,
+  formState,
+  tempWorkout,
+  idleState,
+  addWorkout,
+  modifyTempWorkout,
+  modifyProp,
+}: StatelessCardsContainerProps) => {
+  return (
+    <>
+      <div className="relative z-0 flex min-h-[450px] min-w-[340px] flex-col items-center justify-center rounded-xl border-4 border-dashed border-orange-button500 bg-slate-light100 px-4 dark:bg-slate-dark800">
+        <p className="font-medium italic text-slate-light400">
+          You {"don't"} have any existing workout.
+        </p>
+        <div className="py-4">{DbIcon}</div>
+        <NewWorkoutBtn addingState={addingState} />
+      </div>
+      {formState === "adding" && (
+        <AddingForm
+          tempWorkout={tempWorkout}
+          idleState={idleState}
+          addWorkout={addWorkout}
+          modifyTempWorkout={modifyTempWorkout}
+          modifyProp={modifyProp}
+        />
+      )}
+    </>
+  );
+};
+
+export default StatelessCardsContainer;
