931d337713a3e8813ffc426d03cb30b06d276b43
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-25 18:53:30 +0100

    Refactored for clarity.

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -2,27 +2,24 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { useWorkouts } from "../hooks/useWorkouts";
 import { useEdit } from "../hooks/useEdit";
-import type { WorkoutProps } from "../types/types";
 import BackgroundPattern from "../components/svg-components/background-pattern";
 import {
   FixedLogo,
   OptionsMenu,
-} from "../components/signed-in-components/fixed-components";
-import { CardsContentWrapper } from "../components/wrappers";
-import { CardsContainerHeading } from "../components/headings";
-import CardsContainer from "../components/signed-in-components/cards-container";
-
-const initialWorkouts: WorkoutProps[] = [
-  { id: 1, name: "Upper 1", tooltip: "Good one, could be better." },
-  { id: 2, name: "Upper 2", tooltip: "A little bit better, imo." },
-  { id: 3, name: "Lower 1", tooltip: "The best one so far!" },
-  { id: 4, name: "Lower 2", tooltip: "The best one so far!" },
-  { id: 5, name: "Lower 3", tooltip: "The best one so far!" },
-];
+} from "../components/signed-in-components/options-navbar";
+import { PageContentWrapper } from "../components/wrappers";
+import { CardsHeading } from "../components/headings";
+import { CardsContent } from "../components/signed-in-components/cards-content";
 
 const SignedIn: NextPage = () => {
-  const { workouts, addWorkout, changeWorkout, removeWorkout } =
-    useWorkouts(initialWorkouts);
+  const { workouts, addWorkout, changeWorkout, removeWorkout } = useWorkouts([
+    { id: 1, name: "Upper 1", tooltip: "Good one, could be better." },
+    { id: 2, name: "Upper 2", tooltip: "A little bit better, imo." },
+    { id: 3, name: "Lower 1", tooltip: "The best one so far!" },
+    { id: 4, name: "Lower 2", tooltip: "The best one so far!" },
+    { id: 5, name: "Lower 3", tooltip: "The best one so far!" },
+    { id: 6, name: "Full body", tooltip: "This one is hella hard..." },
+  ]);
   const { isEditing, toggleEdit } = useEdit();
 
   return (
@@ -36,9 +33,9 @@ const SignedIn: NextPage = () => {
         <BackgroundPattern />
         <FixedLogo />
         <OptionsMenu toggleEdit={toggleEdit} />
-        <CardsContentWrapper>
-          <CardsContainerHeading workouts={workouts} />
-          <CardsContainer
+        <PageContentWrapper>
+          <CardsHeading workouts={workouts} />
+          <CardsContent
             workouts={workouts}
             addWorkout={addWorkout}
             changeWorkout={changeWorkout}
@@ -46,7 +43,7 @@ const SignedIn: NextPage = () => {
             isEditing={isEditing}
             toggleEdit={toggleEdit}
           />
-        </CardsContentWrapper>
+        </PageContentWrapper>
       </main>
     </>
   );
