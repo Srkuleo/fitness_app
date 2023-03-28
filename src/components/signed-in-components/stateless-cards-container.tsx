dca3dfc364675f70e543e17b9f5dbe1be7e591d8
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-28 19:45:03 +0200

    .

--- src/components/signed-in-components/stateless-cards-container.tsx
+++ src/components/signed-in-components/stateless-cards-container.tsx
@@ -13,8 +13,8 @@ const StatelessCardsContainer = ({
   modifyProp,
 }: StatelessCardsContainerProps) => {
   return (
-    <>
-      <div className="relative z-0 flex min-h-[450px] min-w-[340px] flex-col items-center justify-center rounded-xl border-4 border-dashed border-orange-button500 bg-slate-light100 px-4 dark:bg-slate-dark800">
+    <div className="relative z-0 mx-auto">
+      <div className="flex h-[500px] w-[380px] flex-col items-center justify-center rounded-xl border-4 border-dashed border-orange-button500 bg-slate-light100 dark:bg-slate-dark800">
         <p className="font-medium italic text-slate-light400">
           You {"don't"} have any existing workout.
         </p>
@@ -30,7 +30,7 @@ const StatelessCardsContainer = ({
           modifyProp={modifyProp}
         />
       )}
-    </>
+    </div>
   );
 };
 
