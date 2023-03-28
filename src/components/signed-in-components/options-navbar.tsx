93fb1701fea10d55dfc7728eafd8f15dc42e3f57
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-28 19:44:56 +0200

    .

--- src/components/signed-in-components/options-navbar.tsx
+++ src/components/signed-in-components/options-navbar.tsx
@@ -13,19 +13,20 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { useOpen } from "../../hooks/useOpen";
 import { useHover } from "../../hooks/useHover";
+import type { OptionsMenuProps } from "../../types/types";
 
 export const FixedLogo = () => {
   return (
-    <div className="fixed z-10 top-9 left-18">
+    <div className="fixed top-9 left-18 z-10">
       <NoteSetLogo />
     </div>
   );
 };
 
-export const OptionsMenu = ({ toggleEdit }: { toggleEdit: () => void }) => {
+export const OptionsMenu = ({ toggleEdit, workouts }: OptionsMenuProps) => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
-  const { isOpen, toggleDropDown } = useOpen();
+  const { isOpen, openDropDown, closeDropDown } = useOpen();
   const { isHovered, toggleHover } = useHover();
 
   useEffect(() => {
@@ -39,80 +40,74 @@ export const OptionsMenu = ({ toggleEdit }: { toggleEdit: () => void }) => {
   if (!mounted) return <></>;
 
   return (
-    <>
-      {isOpen && (
-        <div
-          role="button"
-          onMouseEnter={toggleDropDown}
-          className="fixed top-0 z-10 h-screen w-screen cursor-default"
-        ></div>
-      )}
-      <div className="fixed top-9 right-18 z-10 flex flex-col items-end gap-2 pt-2">
+    <div className="fixed top-9 right-18 z-10 flex flex-col items-end gap-2 pt-2">
+      <button
+        onClick={openDropDown}
+        className="flex items-center gap-3 text-lg font-medium uppercase text-slate-light50"
+      >
+        srkuleo
+        {ArrowDownIcon}
+      </button>
+      <div
+        className={`${
+          isOpen ? "scale-100" : "scale-0"
+        } flex origin-top-right flex-col rounded-lg bg-slate-light50 p-2 text-green-dark700 transition-all duration-200 ease-out`}
+        onMouseLeave={closeDropDown}
+      >
+        <Link
+          className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
+          href="/"
+        >
+          {UserIcon}
+          Profile
+        </Link>
         <button
-          onClick={toggleDropDown}
-          className="flex items-center gap-3 text-lg font-medium uppercase text-slate-light50"
+          disabled={workouts.length < 1}
+          className={`flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50 ${
+            workouts.length < 1 ? "pointer-events-none opacity-20" : ""
+          }`}
+          onClick={() => {
+            toggleEdit();
+          }}
         >
-          srkuleo
-          {ArrowDownIcon}
+          {EditIcon}
+          Edit
         </button>
-        <div
-          className={`${
-            isOpen ? "scale-100" : "scale-0"
-          } flex origin-top-right flex-col rounded-lg bg-slate-light50 p-2 text-green-dark700 transition-all duration-200 ease-out`}
+        <button className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50">
+          {LogsIcon}
+          View logs
+        </button>
+        <button
+          onClick={toggleMode}
+          onMouseEnter={toggleHover}
+          onMouseLeave={toggleHover}
+          className="flex items-center gap-2 rounded-md from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:bg-gradient-to-r hover:text-yellow-text50"
         >
-          <Link
-            className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
-            href="/"
-          >
-            {UserIcon}
-            Profile
-          </Link>
-          <button
-            onClick={() => {
-              toggleEdit();
-              toggleDropDown();
-            }}
-            className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
-          >
-            {EditIcon}
-            Edit
-          </button>
-          <button className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50">
-            {LogsIcon}
-            View logs
-          </button>
-          <button
-            onClick={toggleMode}
-            onMouseEnter={toggleHover}
-            onMouseLeave={toggleHover}
-            className="flex items-center gap-2 rounded-md from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:bg-gradient-to-r hover:text-yellow-text50"
-          >
-            {resolvedTheme === "dark" ? (
-              <>
-                <LightModeIcon size={22} stroke="currentColor" />
-                Light mode
-              </>
-            ) : (
-              <>
-                {isHovered ? (
-                  <DarkModeIcon size={22} color="#cbd5e1" />
-                ) : (
-                  <DarkModeIcon size={22} />
-                )}
-                Dark mode
-              </>
-            )}
-          </button>
-          <div className="my-1 border-b border-green-dark700/20"></div>
-          <Link
-            className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
-            href="/"
-          >
-            {SignOutIcon}
-            Sign out
-          </Link>
-        </div>
+          {resolvedTheme === "dark" ? (
+            <>
+              <LightModeIcon size={22} stroke="currentColor" />
+              Light mode
+            </>
+          ) : (
+            <>
+              {isHovered ? (
+                <DarkModeIcon size={22} color="#cbd5e1" />
+              ) : (
+                <DarkModeIcon size={22} />
+              )}
+              Dark mode
+            </>
+          )}
+        </button>
+        <div className="my-1 border-b border-green-dark700/20"></div>
+        <Link
+          className="flex items-center gap-2 from-green-dark700 via-green-dark700 to-green-main500 py-1 pr-32 pl-2 text-left text-sm uppercase transition-all ease-out hover:rounded-md hover:bg-gradient-to-r hover:text-yellow-text50"
+          href="/"
+        >
+          {SignOutIcon}
+          Sign out
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
