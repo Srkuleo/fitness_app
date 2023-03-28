c1df6c5107f93fa933a5b944de9457e48d638c10
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-28 19:44:35 +0200

    .

---
+++ src/components/signed-in-components/cards-nav-buttons.tsx
@@ -0,0 +1,25 @@
+import type { NavButtonsProps } from "../../types/types";
+import { NextCardIcon, PrevCardIcon } from "../svg-components/svg";
+
+const CardsNavButtons = ({ workouts, prevCard, nextCard }: NavButtonsProps) => {
+  return (
+    <div className="absolute inset-0 flex items-center justify-between">
+      <button
+        className="text-slate-main600 opacity-70 transition-all ease-out hover:-translate-x-1 hover:opacity-100 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
+        onClick={prevCard}
+        disabled={workouts.length < 2}
+      >
+        {PrevCardIcon}
+      </button>
+      <button
+        className="text-slate-main600 opacity-70 transition-all ease-out hover:translate-x-1 hover:opacity-100 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
+        onClick={nextCard}
+        disabled={workouts.length < 2}
+      >
+        {NextCardIcon}
+      </button>
+    </div>
+  );
+};
+
+export default CardsNavButtons;
