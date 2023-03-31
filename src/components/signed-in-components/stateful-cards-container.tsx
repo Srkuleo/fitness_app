0d52b3982eed302ff035940696d2095cbb6f82db
Srdjan <milicevic.srdjan0@gmail.com>
2023-03-31 23:14:57 +0200

    .

--- src/components/signed-in-components/stateful-cards-container.tsx
+++ src/components/signed-in-components/stateful-cards-container.tsx
@@ -3,7 +3,7 @@ import type {
   WorkoutProps,
 } from "../../types/types";
 import { StartBtn } from "../buttons";
-import { WorkoutCardsCarousel } from "../wrappers";
+import { WorkoutCardsCarousel, WorkoutCardLayout } from "../wrappers";
 
 const StatefulCardsContainer = ({
   currIndex,
@@ -27,19 +27,13 @@ const WorkoutCard = ({
   tooltip,
 }: Pick<WorkoutProps, "name" | "tooltip">) => {
   return (
-    <div className="min-h-[520px] min-w-[400px]">
-      <div className="relative h-full w-full overflow-hidden rounded-[24px] border-4 border-orange-button600 bg-slate-light50 dark:border-orange-button500 dark:bg-gradient-to-tr dark:from-slate-main600 dark:to-slate-dark800">
-        <div className="flex flex-col items-center gap-4 p-4">
-          <p className="text-2xl font-medium text-slate-main600 dark:text-slate-light200">
-            {name}
-          </p>
-          <p className="text-sm text-slate-light400">{tooltip}</p>
-          <StartBtn />
-        </div>
-        <div className="absolute top-[350px] left-[224px] h-[290px] w-[290px] rounded-full bg-[#caeed2] dark:bg-[#69ab69]/70"></div>
-        <div className="absolute top-[416px] left-[290px] h-[160px] w-[160px] rounded-full bg-[#b4c6b1] dark:bg-[#4a784a]/80"></div>
-      </div>
-    </div>
+    <WorkoutCardLayout>
+      <p className="text-2xl font-medium text-slate-main600 dark:text-slate-light200">
+        {name}
+      </p>
+      <p className="text-sm text-slate-light400">{tooltip}</p>
+      <StartBtn />
+    </WorkoutCardLayout>
   );
 };
 
