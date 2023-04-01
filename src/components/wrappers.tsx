8a15ebce771c992e7e93f9f762bf86a30ec3d39c
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-01 23:07:21 +0200

    Tweak gradient inside workout card

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -16,8 +16,8 @@ export const OAuthBtnWrapper = ({ children }: WrapperChild) => {
       rounded-xl border-2 border-green-main500 bg-slate-light50 
       py-2 text-sm font-semibold text-slate-main600 
       transition-all ease-in hover:translate-x-1 
-    hover:bg-slate-light200/90 dark:bg-slate-dark800 
-    dark:text-yellow-text50 dark:hover:bg-slate-light500/50"
+      hover:bg-slate-light200/90 dark:bg-slate-dark800 
+      dark:text-yellow-text50 dark:hover:bg-slate-light500/50"
     >
       {children}
     </button>
@@ -52,17 +52,11 @@ export const WorkoutCardsCarousel = ({
 export const WorkoutCardLayout = ({ children }: WrapperChild) => {
   return (
     <div
-      className="relative min-h-[520px] min-w-[400px] overflow-hidden 
-      rounded-2xl border-4 border-orange-button600 bg-gradient-to-tl 
-    odd:from-slate-light50 odd:to-slate-light100 
-    even:bg-slate-light50
-    dark:border-orange-button500 
-    dark:odd:from-slate-light500 from-60% dark:odd:to-slate-dark800 to-95%
-    dark:even:bg-slate-dark800"
+      className="workout-card-layout"
     >
       <div className="flex flex-col items-center gap-4 p-4">{children}</div>
-      <div className="absolute top-[350px] left-[224px] h-[290px] w-[290px] rounded-full bg-[#caeed2] dark:bg-[#69ab69]/70" />
-      <div className="absolute top-[416px] left-[290px] h-[160px] w-[160px] rounded-full bg-[#b6e3ae] dark:bg-[#4a784a]/80" />
+      <div className="absolute left-[224px] top-[350px] h-[290px] w-[290px] rounded-full bg-[#caeed2] dark:bg-[#69ab69]/70" />
+      <div className="absolute left-[290px] top-[416px] h-[160px] w-[160px] rounded-full bg-[#b6e3ae] dark:bg-[#4a784a]/80" />
     </div>
   );
 };
