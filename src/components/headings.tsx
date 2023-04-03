7baf67af6782bf94556495ade7742cd786bd2535
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-03 11:45:33 +0200

    .

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -8,7 +8,7 @@ export const SignContentHeading = ({ page }: { page: string }) => {
           ? "Sign in and start your workout."
           : "A minimalistic approach to tracking your workout progress."}
       </p>
-      <p className="mt-4 mb-3 italic text-slate-main600 dark:text-slate-light300">
+      <p className="mb-3 mt-4 italic text-slate-main600 dark:text-slate-light300">
         continue with
       </p>
     </div>
@@ -21,7 +21,7 @@ export const CardsHeading = ({ workouts }: { workouts: WorkoutProps[] }) => {
       <h1 className="mb-18 text-5xl font-bold text-orange-button500">
         Welcome, Srkuleo.
       </h1>
-      <h3 className="text-2xl font-semibold text-slate-dark700 dark:text-yellow-text50">
+      <h3 className="text-2xl font-semibold text-slate-main600 dark:text-yellow-text50">
         {workouts.length > 0 ? "Swipe left or right to choose a workout" : ""}
       </h3>
     </div>
