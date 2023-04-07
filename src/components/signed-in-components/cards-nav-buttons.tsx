a89799f9fdf3fd6b360c933c176fc4bfe3bdc119
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-07 23:38:52 +0200

    Introduce naming changes

--- src/components/signed-in-components/cards-nav-buttons.tsx
+++ src/components/signed-in-components/cards-nav-buttons.tsx
@@ -1,25 +1,22 @@
-import type { NavButtonsProps } from "../../types/types";
-import { NextCardArrow, PrevCardArrow } from "../svg-components/svg";
+import type { CardsNavButtonsProps } from "../../types/types";
 
-const CardsNavButtons = ({ workouts, prevCard, nextCard }: NavButtonsProps) => {
+export const CardsNavButtons = ({
+  workouts,
+  InFocus,
+  jumpToCard,
+}: CardsNavButtonsProps) => {
   return (
-    <div className="absolute inset-0 flex items-center justify-between">
-      <button
-        className="text-slate-main600 opacity-70 transition-all ease-out hover:-translate-x-1 hover:opacity-100 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
-        onClick={prevCard}
-        disabled={workouts.length < 2}
-      >
-        {PrevCardArrow}
-      </button>
-      <button
-        className="text-slate-main600 opacity-70 transition-all ease-out hover:translate-x-1 hover:opacity-100 disabled:pointer-events-none disabled:opacity-20 dark:text-slate-light100"
-        onClick={nextCard}
-        disabled={workouts.length < 2}
-      >
-        {NextCardArrow}
-      </button>
+    <div className="flex items-center justify-center gap-smallButton">
+      {workouts.map((workout, i) => (
+        <div
+          key={workout.id}
+          role="button"
+          className={`h-3 w-3 rounded-full bg-slate-light500 dark:bg-slate-light100 ${
+            InFocus === i ? "p-2" : "opacity-40"
+          }`}
+          onClick={() => jumpToCard(i)}
+        ></div>
+      ))}
     </div>
   );
 };
-
-export default CardsNavButtons;
