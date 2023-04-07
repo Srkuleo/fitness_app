247d980379f561b8843cf114b6b0d29b4ef93821
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-07 23:46:42 +0200

    Adds context for id

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { useState } from "react";
+import { IdContext } from "../utils/idContext";
 import { ThemeProvider } from "next-themes";
 import "../styles/global.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const [id, setId] = useState(4);
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <IdContext.Provider value={{ id, setId }}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </IdContext.Provider>
   );
 };
 
