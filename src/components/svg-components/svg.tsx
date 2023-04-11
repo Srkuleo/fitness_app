ab1fe881bcef05dfefb358619a31c79c18552420
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-11 23:10:31 +0200

    New SVG icons for nav arrows removed few unused

--- src/components/svg-components/svg.tsx
+++ src/components/svg-components/svg.tsx
@@ -357,50 +357,6 @@ export const DoneIcon = (
   </svg>
 );
 
-export const AddingFormIcon = () => {
-  return (
-    <div className="mx-auto mb-2 max-w-fit rounded-full bg-slate-light500 p-3">
-      <svg
-        className="h-dropdown-icon w-dropdown-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-        />
-      </svg>
-    </div>
-  );
-};
-
-export const ChangingFormIcon = () => {
-  return (
-    <div className="mx-auto mb-2 max-w-fit rounded-full bg-slate-light500 p-3">
-      <svg
-        className="h-dropdown-icon w-dropdown-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-        />
-      </svg>
-    </div>
-  );
-};
-
 export const DbIcon = (
   <svg
     className="h-[60px] w-[60px] text-slate-light300/70"
@@ -432,36 +388,54 @@ export const DbIcon = (
 
 export const PrevCardArrow = (
   <svg
-    className="h-7 w-7 opacity-60 transition-all ease-out group-hover:-translate-x-0.5 group-hover:opacity-100"
+    className="h-6 w-6 opacity-50 transition-all ease-out hover:-translate-x-0.5 hover:opacity-100"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={2}
+    strokeWidth={2.5}
     stroke="currentColor"
     aria-hidden="true"
   >
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M15.75 19.5L8.25 12l7.5-7.5"
+      d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
     />
   </svg>
 );
 
 export const NextCardArrow = (
   <svg
-    className="h-7 w-7 opacity-60 transition-all ease-out group-hover:translate-x-0.5 group-hover:opacity-100"
+    className="h-6 w-6 opacity-50 transition-all ease-out hover:translate-x-0.5 hover:opacity-100"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={2}
+    strokeWidth={2.5}
     stroke="currentColor"
     aria-hidden="true"
   >
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M8.25 4.5l7.5 7.5-7.5 7.5"
+      d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+    />
+  </svg>
+);
+
+export const ExclamationTriangleIcon = (
+  <svg
+    className="text-red-600 h-6 w-6"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
     />
   </svg>
 );
