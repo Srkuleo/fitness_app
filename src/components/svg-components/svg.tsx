5490278bba7da072d4fd97114e9e0f5f4ac5c7d2
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-15 02:09:11 +0200

    Small tweaks to arrow down icon

--- src/components/svg-components/svg.tsx
+++ src/components/svg-components/svg.tsx
@@ -217,7 +217,7 @@ export const ArrowDownIcon = ({
     className={`
       h-4 w-4 transition-all duration-300
       ${isOpenDropDown && "rotate-180 ease-in"}
-      ${isOpenDropDown && "ease-out group-hover:translate-y-0.5"}`}
+      ${!isOpenDropDown && "ease-out group-hover:translate-y-0.5"}`}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
