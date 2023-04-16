a84948a85ad86fce581d1e72d89f06336be7072f
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-16 18:56:47 +0200

    Adds expanding animation to buttons

--- src/components/signed-in-components/edit-overlay.tsx
+++ src/components/signed-in-components/edit-overlay.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { EditIcon, RemoveIcon } from "../svg-components/svg";
-import type { EditOverlayProps } from "../../types/types";
 import { AnimatePresence, motion } from "framer-motion";
-import { RemoveModal } from "./remove-modal";
+import type { EditOverlayProps } from "../../types/types";
+import { buttonsVariant } from "../../utils/variables";
+import { EditIcon, RemoveIcon } from "../svg-components/svg";
+import { RemoveModal } from "./modals";
 
 export const EditOverlay = ({
   handleEditForm,
@@ -11,7 +12,11 @@ export const EditOverlay = ({
   removeWorkout,
   switchInFocus,
 }: EditOverlayProps) => {
-  const [showModal, setShowModal] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [buttons, setButtons] = useState({
+    remove: false,
+    edit: false,
+  });
 
   const overlayVariant = {
     hidden: {
@@ -19,11 +24,11 @@ export const EditOverlay = ({
     },
     "fade-in": {
       opacity: 1,
-      transition: { duration: 0.2, ease: "easeOut" },
+      transition: { duration: 0.1, ease: "easeOut" },
     },
     "fade-out": {
       opacity: 0,
-      transition: { duration: 0.1, ease: "easeIn" },
+      transition: { duration: 0.05, ease: "easeIn" },
     },
   };
 
@@ -41,11 +46,26 @@ export const EditOverlay = ({
         initial="hidden"
         animate="fade-in"
         exit="fade-out"
-        className="absolute right-4 top-4 z-10 rounded-full bg-red-removeBtn100 p-2 text-red-button700 shadow-md ring-1 ring-red-button400 transition-all ease-out hover:bg-red-button500 hover:text-slate-light50 hover:ring-red-button700"
+        className="absolute right-4 top-4 z-10 flex items-center gap-2 rounded-full bg-red-removeBtn100 p-2 text-red-button700 shadow-md ring-1 ring-red-button400 hover:bg-red-button500 hover:text-slate-light50 hover:ring-red-button700"
+        onHoverStart={() => setButtons({ ...buttons, remove: true })}
+        onHoverEnd={() => setButtons({ ...buttons, remove: false })}
         onClick={() => {
-          setShowModal(true);
+          setShowRemoveModal(true);
         }}
       >
+        <AnimatePresence>
+          {buttons.remove && (
+            <motion.span
+              variants={buttonsVariant}
+              initial="hidden"
+              animate="show"
+              exit="hide"
+              className="overflow-hidden pl-2 text-xs"
+            >
+              Remove
+            </motion.span>
+          )}
+        </AnimatePresence>
         {RemoveIcon}
       </motion.button>
       <motion.button
@@ -53,18 +73,33 @@ export const EditOverlay = ({
         initial="hidden"
         animate="fade-in"
         exit="fade-out"
-        className="center absolute left-[50%] top-[50%] z-10 rounded-full bg-green-light100 p-2 text-green-dark700 shadow-md ring-1 ring-green-light400 transition-all ease-out hover:bg-green-dark600 hover:text-slate-light50 hover:ring-green-dark700"
+        className="center absolute left-[50%] top-[50%] z-10 flex items-center gap-2 rounded-full bg-green-light100 p-2 text-green-dark700 shadow-md ring-1 ring-green-light400 hover:bg-green-dark600 hover:text-slate-light50 hover:ring-green-dark700"
+        onHoverStart={() => setButtons({ ...buttons, edit: true })}
+        onHoverEnd={() => setButtons({ ...buttons, edit: false })}
         onClick={() => {
           handleEditForm(workout.id);
           closeEditOverlay();
         }}
       >
         <EditIcon className="h-8 w-8" strokeWidth={2} />
+        <AnimatePresence>
+          {buttons.edit && (
+            <motion.span
+              variants={buttonsVariant}
+              initial="hidden"
+              animate="show"
+              exit="hide"
+              className="overflow-hidden pr-2"
+            >
+              Edit
+            </motion.span>
+          )}
+        </AnimatePresence>
       </motion.button>
       <AnimatePresence>
-        {showModal && (
+        {showRemoveModal && (
           <RemoveModal
-            onClose={() => setShowModal(false)}
+            onClose={() => setShowRemoveModal(false)}
             workout={workout}
             removeWorkout={removeWorkout}
             switchInFocus={switchInFocus}
