ff325c011b333b4c9b33399a8c8873e38e96808a
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-16 18:55:48 +0200

    Adds expanding animation

--- src/components/signed-in-components/card-edit-buttons.tsx
+++ src/components/signed-in-components/card-edit-buttons.tsx
@@ -1,7 +1,9 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useIncrementId } from "../../hooks/useIncrementId";
-import { initWorkout } from "../../utils/variables";
-import { AddIcon, DoneIcon } from "../svg-components/svg";
 import type { CardEditButtonsProps } from "../../types/types";
+import { initWorkout, buttonsVariant } from "../../utils/variables";
+import { AddIcon, DoneIcon } from "../svg-components/svg";
+import { useState } from "react";
 
 export const CardEditButttons = ({
   workouts,
@@ -11,30 +13,62 @@ export const CardEditButttons = ({
   handleEditForm,
   toggleAdding,
 }: CardEditButtonsProps) => {
+  const [expandAddBtn, setExpandAddBtn] = useState(false);
+  const [expandDoneBtn, setExpandDoneBtn] = useState(false);
   const { id, incrementId } = useIncrementId();
 
   return (
     <div className="flex justify-around">
-      <button
-        className="rounded-full bg-slate-light300 p-2 text-slate-main600 shadow-sm transition-all ease-out hover:-translate-x-1 dark:bg-slate-light400 dark:text-slate-light50"
-        onClick={() => {
-          closeEditOverlay();
-          toggleAdding();
-          addWorkout({ ...initWorkout, id: id });
-          handleEditForm(id);
-          console.log(id);
-          incrementId();
-          jumpToCard(workouts.length);
-        }}
-      >
-        {AddIcon}
-      </button>
-      <button
-        className="rounded-full bg-slate-light300 p-2 text-slate-main600 shadow-sm transition-all ease-out hover:translate-x-1 dark:bg-slate-light400 dark:text-slate-light50"
-        onClick={closeEditOverlay}
-      >
-        {DoneIcon}
-      </button>
+      <AnimatePresence>
+        <motion.button
+          className="flex items-center gap-2 rounded-full bg-slate-light300 p-2 text-slate-main600 shadow-sm dark:bg-slate-light400 dark:text-slate-light50"
+          onHoverStart={() => setExpandAddBtn(true)}
+          onHoverEnd={() => setExpandAddBtn(false)}
+          onClick={() => {
+            closeEditOverlay();
+            toggleAdding();
+            addWorkout({ ...initWorkout, id: id });
+            handleEditForm(id);
+            console.log(id);
+            incrementId();
+            jumpToCard(workouts.length);
+          }}
+        >
+          {AddIcon}
+          {expandAddBtn && (
+            <motion.span
+              variants={buttonsVariant}
+              initial="hidden"
+              animate="show"
+              exit="hide"
+              className="overflow-hidden text-sm"
+            >
+              Add
+            </motion.span>
+          )}
+        </motion.button>
+      </AnimatePresence>
+      <AnimatePresence>
+        <motion.button
+          className="flex items-center gap-2 rounded-full bg-slate-light300 p-2 text-slate-main600 shadow-sm dark:bg-slate-light400 dark:text-slate-light50"
+          onHoverStart={() => setExpandDoneBtn(true)}
+          onHoverEnd={() => setExpandDoneBtn(false)}
+          onClick={closeEditOverlay}
+        >
+          {DoneIcon}
+          {expandDoneBtn && (
+            <motion.span
+              variants={buttonsVariant}
+              initial="hidden"
+              animate="show"
+              exit="hide"
+              className="overflow-hidden text-sm"
+            >
+              Done
+            </motion.span>
+          )}
+        </motion.button>
+      </AnimatePresence>
     </div>
   );
 };
