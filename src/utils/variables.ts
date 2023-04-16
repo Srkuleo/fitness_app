b49ab781fd89a2f722ca9568a36a5f9a71d484df
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-16 19:04:28 +0200

    Adds reusable framer motion variant

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -16,3 +16,19 @@ export const initWorkout: WorkoutProps = {
   title: "New title",
   description: "New description",
 };
+
+//Variant for framer motion animations - buttons signed in page
+export const buttonsVariant = {
+  hidden: { width: 0, opacity: 0 },
+  show: {
+    width: "auto",
+    opacity: 1,
+    originX: 0,
+    transition: { duration: 0.05, ease: "easeOut" },
+  },
+  hide: {
+    width: 0,
+    opacity: 0,
+    transition: { duration: 0.02, ease: "easeIn" },
+  },
+};
