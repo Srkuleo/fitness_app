b7e2b6df6fe13ae9728fb2a6ac4881c401c78fe1
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-19 22:32:46 +0200

    Moves logic further down the tree

--- src/components/sign-components/nav-bar.tsx
+++ src/components/sign-components/nav-bar.tsx
@@ -1,35 +1,14 @@
 import NoteSetLogo from "../svg-components/note-set-logo";
 import { GitHubButton, ModeButton } from "../buttons";
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
 
 const NavBar = () => {
   return (
-    <div className="mt-4 mb-36 flex justify-between border-b-2 border-slate-light400 pb-2 dark:border-slate-light500">
+    <div className="mb-36 py-2  flex justify-between bg-gradient-to-r from-orange-button600/70 to-green-dark600">
       <NoteSetLogo />
-      <NavBarButtons />
-    </div>
-  );
-};
-
-const NavBarButtons = () => {
-  const { resolvedTheme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  function toggleMode() {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
-  }
-
-  if (!mounted) return <></>;
-
-  return (
-    <div className="flex items-center">
-      <GitHubButton />
-      <ModeButton theme={resolvedTheme} toggleMode={toggleMode} />
+      <div className="flex items-center">
+        <GitHubButton />
+        <ModeButton />
+      </div>
     </div>
   );
 };
