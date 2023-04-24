411807198bb89c479511f383bda5b47777848b78
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-24 22:37:32 +0200

    Refactors navbar

--- src/components/sign-components/nav-bar.tsx
+++ src/components/sign-components/nav-bar.tsx
@@ -3,9 +3,9 @@ import { GitHubButton, ModeButton } from "../buttons";
 
 const NavBar = () => {
   return (
-    <div className="mb-36 py-2  flex justify-between bg-gradient-to-r from-orange-button600/70 to-green-dark600">
+    <div className="mb-36 flex justify-between bg-gradient-to-r from-orange-button600 via-green-main500 to-green-dark600 dark:from-orange-button600 dark:via-green-dark600 dark:to-green-dark800 px-4 py-2 md:border-b-2 md:border-slate-light400 md:from-tp md:dark:from-tp md:px-0">
       <NoteSetLogo />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         <GitHubButton />
         <ModeButton />
       </div>
