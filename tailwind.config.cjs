df448eb92a8396b76717292d9268b795f63bab04
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-26 23:44:51 +0200

    New screens and white color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+/**  @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     colors: {
+      white: "#fff",
       black: "#000",
       tp: "transparent",
       current: "currentColor",
