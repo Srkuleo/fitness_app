cb203ffef1946b442a7bf7aad2a34341597f73fc
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-26 23:45:21 +0200

    Adds responsiveness

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -2,28 +2,28 @@ import type { WorkoutProps } from "../types/types";
 
 export const SignContentHeading = ({ page }: { page: string }) => {
   return (
-    <div>
+    <>
       <p className="text-xl font-bold text-slate-main600 dark:text-yellow-text50">
         {page === "sign in"
-          ? "Sign in and start your workout."
-          : "A minimalistic approach to tracking your workout progress."}
+          ? "Sign in, start your workout."
+          : "Sign up, enjoy tracking."}
       </p>
       <p className="mb-3 mt-4 italic text-slate-main600 dark:text-slate-light300">
         continue with
       </p>
-    </div>
+    </>
   );
 };
 
 export const CardsHeading = ({ workouts }: { workouts: WorkoutProps[] }) => {
   return (
-    <div className="text-center">
-      <h1 className="mb-18 text-5xl font-bold text-orange-button500">
+    <>
+      <h1 className="mb-4 text-3xl font-bold text-orange-button500 md:mb-8 md:text-5xl">
         Welcome, Srkuleo.
       </h1>
-      <h3 className="text-2xl font-semibold text-slate-main600 dark:text-yellow-text50">
+      <h3 className="mb-2 text-lg font-semibold text-slate-main600 dark:text-yellow-text50 md:text-2xl">
         {workouts.length > 0 ? "Swipe left or right to choose a workout" : ""}
       </h3>
-    </div>
+    </>
   );
 };
