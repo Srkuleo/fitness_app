9a99ba4be1eaed6e21acb4c25d0b2479cae8f601
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-26 23:46:21 +0200

    Resuable navbar

--- src/components/sign-components/nav-bar.tsx
+++ src/components/sign-components/nav-bar.tsx
@@ -1,16 +1,28 @@
-import NoteSetLogo from "../svg-components/note-set-logo";
+import type { WrapperProp } from "../../types/types";
 import { GitHubButton, ModeButton } from "../buttons";
+import { NoteSetLogoSvg } from "../svg";
 
-const NavBar = () => {
+export const NavBar = ({ children }: Partial<WrapperProp>) => {
   return (
-    <div className="mb-36 flex justify-between bg-gradient-to-r from-orange-button600 via-green-main500 to-green-dark600 dark:from-orange-button600 dark:via-green-dark600 dark:to-green-dark800 px-4 py-2 md:border-b-2 md:border-slate-light400 md:from-tp md:dark:from-tp md:px-0">
-      <NoteSetLogo />
+    <div className="mx-auto flex justify-between bg-gradient-to-r from-orange-button600 via-green-main500 to-green-dark600 px-4 py-2 dark:from-orange-dark700 dark:to-green-dark700 md:w-4/6 md:border-b-2 md:border-slate-light400 md:from-tp md:px-0 md:dark:from-tp lg:w-3/6">
+      <NoteSet />
       <div className="flex items-center gap-2">
         <GitHubButton />
         <ModeButton />
+        {children}
       </div>
     </div>
   );
 };
 
-export default NavBar;
+const NoteSet = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <div className="hidden md:block">{NoteSetLogoSvg}</div>
+      <span className="text-xl font-bold text-slate-light50 sm:text-2xl md:text-green-main500">
+        Note
+        <span className="font-medium md:text-slate-light500">Set</span>
+      </span>
+    </div>
+  );
+};
