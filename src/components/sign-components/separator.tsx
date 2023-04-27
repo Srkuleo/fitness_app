7795eafcb972929e776731a0689ac74cdf987e97
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-27 21:50:12 +0200

    Adds responsiveness

--- src/components/sign-components/separator.tsx
+++ src/components/sign-components/separator.tsx
@@ -2,7 +2,9 @@ const Separator = () => {
   return (
     <div className="mx-2 my-3 flex items-center gap-1">
       <div className="w-32 border-b border-green-main500/70"></div>
-      <p className="italic text-slate-main600 dark:text-slate-light300">or</p>
+      <p className="text-sm italic text-slate-main600 dark:text-slate-light300 xs:text-base">
+        or
+      </p>
       <div className="w-32 border-b border-green-main500/70"></div>
     </div>
   );
