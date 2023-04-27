e6ca6a9888bd2203ad75380c7569d616296db9b0
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-27 21:50:00 +0200

    Reusable navbar

--- src/components/sign-components/nav-bar.tsx
+++ src/components/sign-components/nav-bar.tsx
@@ -4,7 +4,13 @@ import { NoteSetLogoSvg } from "../svg";
 
 export const NavBar = ({ children }: Partial<WrapperProp>) => {
   return (
-    <div className="mx-auto flex justify-between bg-gradient-to-r from-orange-button600 via-green-main500 to-green-dark600 px-4 py-2 dark:from-orange-dark700 dark:to-green-dark700 md:w-4/6 md:border-b-2 md:border-slate-light400 md:from-tp md:px-0 md:dark:from-tp lg:w-3/6">
+    <div
+      className="fixed left-0 right-0 top-0 z-10 flex justify-between bg-gradient-to-r from-orange-button600 
+    via-green-main500 to-green-dark600 px-4 py-2 dark:from-orange-dark700 
+    dark:to-green-dark700 md:relative 
+    md:mx-auto md:w-4/6 md:border-b-2 md:border-slate-light400 
+    md:from-tp md:px-0 md:dark:from-tp lg:w-3/6 "
+    >
       <NoteSet />
       <div className="flex items-center gap-2">
         <GitHubButton />
