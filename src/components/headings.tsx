12ba090e558952d443db5b4721e58ab61cf96a8d
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-27 21:49:05 +0200

    Refactoring

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -3,12 +3,12 @@ import type { WorkoutProps } from "../types/types";
 export const SignContentHeading = ({ page }: { page: string }) => {
   return (
     <>
-      <p className="text-xl font-bold text-slate-main600 dark:text-yellow-text50">
+      <p className="mb-3 text-lg font-bold text-slate-main600 dark:text-yellow-text50 xs:text-xl">
         {page === "sign in"
           ? "Sign in, start your workout."
           : "Sign up, enjoy tracking."}
       </p>
-      <p className="mb-3 mt-4 italic text-slate-main600 dark:text-slate-light300">
+      <p className="mb-3 text-sm italic text-slate-main600 dark:text-slate-light300 xs:text-base">
         continue with
       </p>
     </>
@@ -18,10 +18,10 @@ export const SignContentHeading = ({ page }: { page: string }) => {
 export const CardsHeading = ({ workouts }: { workouts: WorkoutProps[] }) => {
   return (
     <>
-      <h1 className="mb-4 text-3xl font-bold text-orange-button500 md:mb-8 md:text-5xl">
+      <h1 className="mb-4 text-3xl font-bold text-orange-button500 md:mb-8 md:text-4xl">
         Welcome, Srkuleo.
       </h1>
-      <h3 className="mb-2 text-lg font-semibold text-slate-main600 dark:text-yellow-text50 md:text-2xl">
+      <h3 className="mb-2 text-lg font-semibold text-slate-main600 dark:text-yellow-text50 md:text-xl">
         {workouts.length > 0 ? "Swipe left or right to choose a workout" : ""}
       </h3>
     </>
