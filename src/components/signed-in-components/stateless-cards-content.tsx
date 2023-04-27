03c27aa6a36879fda96001c177fd3d37c25c5348
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-27 21:53:30 +0200

    Refactored to be responsive

--- src/components/signed-in-components/stateless-cards-content.tsx
+++ src/components/signed-in-components/stateless-cards-content.tsx
@@ -6,40 +6,36 @@ import { AddIcon, DbIcon } from "../svg";
 
 export const StatelessCardContent = ({
   toggleAdding,
-  closeEditOverlay,
   addWorkout,
   handleEditForm,
 }: StatelessCardContentProps) => {
   const { id, incrementId } = useIncrementId();
 
   return (
-    <div className="mx-auto">
-      <div className="relative h-[500px] w-[380px] rounded-xl border-4 border-dashed border-orange-button500 bg-slate-light100 dark:bg-slate-dark800">
-        <div className="relative z-10 mt-12 flex flex-col items-center">
-          <p className="font-medium italic text-slate-light400">
-            You {"don't"} have any existing workout.
-          </p>
-        </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          {DbIcon}
-        </div>
-        <div className="absolute bottom-12 right-4">
-          <motion.button
-            whileTap={{ scale: 0.95 }}
-            className="add-workout-btn"
-            onClick={() => {
-              toggleAdding();
-              closeEditOverlay();
-              addWorkout({ ...initWorkout, id: id });
-              handleEditForm(id);
-              incrementId();
-            }}
-          >
-            {AddIcon}
-            New workout
-          </motion.button>
-        </div>
+    <div
+      className="relative mx-auto min-h-[540px] max-w-[370px] rounded-lg 
+    border-2 border-dashed border-orange-button500 bg-white 
+    pt-12 dark:bg-slate-dark700 xs:rounded-xl md:border-4"
+    >
+      <p className="font-medium italic text-slate-light400">
+        You don&apos;t have any existing workout.
+      </p>
+      <div className="absolute inset-0 flex items-center justify-center">
+        {DbIcon}
       </div>
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        className="add-workout-btn absolute bottom-12 right-4"
+        onClick={() => {
+          toggleAdding();
+          addWorkout({ ...initWorkout, id: id });
+          handleEditForm(id);
+          incrementId();
+        }}
+      >
+        {AddIcon}
+        New workout
+      </motion.button>
     </div>
   );
 };
