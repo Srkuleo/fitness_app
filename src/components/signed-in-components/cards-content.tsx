d2a9920d8cdee96414662938154cba10a4845211
Srdjan <milicevic.srdjan0@gmail.com>
2023-04-27 21:51:34 +0200

    Remove edit overlay, total editing system overhaul

--- src/components/signed-in-components/cards-content.tsx
+++ src/components/signed-in-components/cards-content.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useEditForm } from "../../hooks/useEditForm";
 import { useCardInFocus } from "../../hooks/useCardInFocus";
 import type { CardsContentProps } from "../../types/types";
@@ -5,17 +6,12 @@ import { WorkoutCardsCarousel } from "../wrappers";
 import { StatelessCardContent } from "./stateless-cards-content";
 import { WorkoutCard } from "./workout-card";
 import { CarouselNavigation } from "../buttons";
-import { CardEditButttons } from "./card-edit-buttons";
-import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
 
 export const CardsContent = ({
   workouts,
   addWorkout,
   changeWorkout,
   removeWorkout,
-  isOpenEditOverlay,
-  closeEditOverlay,
 }: CardsContentProps) => {
   const [isAdding, setIsAdding] = useState(false);
   const { cardInFocus, prevCard, nextCard, switchInFocus, jumpToCard } =
@@ -29,14 +25,13 @@ export const CardsContent = ({
       <StatelessCardContent
         addWorkout={addWorkout}
         handleEditForm={handleEditForm}
-        closeEditOverlay={closeEditOverlay}
         toggleAdding={() => setIsAdding(true)}
       />
     );
   }
 
   return (
-    <div className="flex flex-col gap-2">
+    <>
       <WorkoutCardsCarousel cardInFocus={cardInFocus}>
         {workouts.map((workout) => (
           <WorkoutCard
@@ -47,13 +42,12 @@ export const CardsContent = ({
             changeWorkout={changeWorkout}
             removeWorkout={removeWorkout}
             switchInFocus={switchInFocus}
-            isOpenEditOverlay={isOpenEditOverlay}
-            closeEditOverlay={closeEditOverlay}
             isAdding={isAdding}
             toggleAdding={() => setIsAdding(false)}
           />
         ))}
       </WorkoutCardsCarousel>
+
       <CarouselNavigation
         cardInFocus={cardInFocus}
         workouts={workouts}
@@ -61,18 +55,6 @@ export const CardsContent = ({
         prevCard={prevCard}
         nextCard={nextCard}
       />
-      <AnimatePresence>
-        {isOpenEditOverlay && (
-          <CardEditButttons
-            workouts={workouts}
-            addWorkout={addWorkout}
-            jumpToCard={jumpToCard}
-            closeEditOverlay={closeEditOverlay}
-            handleEditForm={handleEditForm}
-            toggleAdding={() => setIsAdding(true)}
-          />
-        )}
-      </AnimatePresence>
-    </div>
+    </>
   );
 };
