1c7327a43b6382094964363eddc56e1c886bc5cd
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-03 23:55:10 +0200

    .

--- src/components/sign-components/sign-content.tsx
+++ src/components/sign-components/sign-content.tsx
@@ -6,7 +6,7 @@ import LinkQuestions from "./link-questions";
 
 export const SignContent = ({ page }: { page: "sign in" | "sign up" }) => {
   return (
-    <div className="mx-auto mt-24 max-w-[260px] xs:mt-44 xs:max-w-[295px] md:mt-28">
+    <div className="mt-24 max-w-[260px] xs:max-w-[295px]">
       <SignContentHeading page={page} />
       <OAuth />
       <Separator />
