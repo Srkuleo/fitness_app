d8c7c9935eee4012e8eefffd1ef8eba4079f112d
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-03 23:57:13 +0200

    Few types removed, will revisit

--- src/types/types.ts
+++ src/types/types.ts
@@ -23,51 +23,18 @@ export interface OptionsMenuProps {
   closeMenu: () => void;
 }
 
-export interface CardsContentProps {
+export interface PageContentProps {
   workouts: WorkoutProps[];
   addWorkout: (workout: WorkoutProps) => void;
   changeWorkout: (editedWorkout: WorkoutProps | undefined) => void;
   removeWorkout: (id: number) => void;
+  editBar: boolean;
+  toggleEditBar: () => void;
 }
-
-export interface StatelessCardContentProps
-  extends Pick<CardsContentProps, "addWorkout"> {
-  handleEditForm: (id: number | undefined) => void;
-  toggleAdding: () => void;
-}
-
-export interface WorkoutCardProps
-  extends Omit<CardsContentProps, "workouts" | "addWorkout"> {
-  workout: WorkoutProps;
-  switchInFocus: () => void;
-  editForm: number | undefined;
-  handleEditForm: (id: number | undefined) => void;
-  isAdding: boolean;
-  toggleAdding: () => void;
-}
-
-export type EditFormProps = Omit<WorkoutCardProps, "editForm">;
 
 export interface CarouselNavigationProps
-  extends Pick<CardsContentProps, "workouts"> {
+  extends Pick<PageContentProps, "workouts"> {
   cardInFocus: number;
-  jumpToCard: (index: number) => void;
   prevCard: () => void;
   nextCard: () => void;
-}
-
-export type CardEditButtonsProps = Pick<
-  CardsContentProps,
-  "workouts" | "addWorkout"
-> & {
-  jumpToCard: (index: number) => void;
-  handleEditForm: (id: number | undefined) => void;
-  toggleAdding: () => void;
-};
-
-export interface RemoveModalProps {
-  workout: WorkoutProps;
-  onClose: () => void;
-  removeWorkout: (id: number) => void;
-  switchInFocus: () => void;
 }
