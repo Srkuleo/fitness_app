fdac2fc86670a7c32aa88a01ed7ada2b96fdd727
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-03 23:54:30 +0200

    .

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -1,5 +1,3 @@
-import type { WorkoutProps } from "../types/types";
-
 export const SignContentHeading = ({ page }: { page: string }) => {
   return (
     <>
@@ -11,19 +9,6 @@ export const SignContentHeading = ({ page }: { page: string }) => {
       <p className="mb-3 text-sm italic text-slate-main600 dark:text-slate-light300 xs:text-base">
         continue with
       </p>
-    </>
-  );
-};
-
-export const CardsHeading = ({ workouts }: { workouts: WorkoutProps[] }) => {
-  return (
-    <>
-      <h1 className="mb-4 text-3xl font-bold text-orange-button500 md:mb-8 md:text-4xl">
-        Welcome, Srkuleo.
-      </h1>
-      <h3 className="mb-2 text-lg font-semibold text-slate-main600 dark:text-yellow-text50 md:text-xl">
-        {workouts.length > 0 ? "Swipe left or right to choose a workout" : ""}
-      </h3>
     </>
   );
 };
