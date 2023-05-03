50532a60293a1a9cd3e7113fbcff1dc625224d75
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-03 23:54:54 +0200

    .

--- src/components/sign-components/link-questions.tsx
+++ src/components/sign-components/link-questions.tsx
@@ -2,27 +2,33 @@ import Link from "next/link";
 
 const LinkQuestions = ({ page }: { page: "sign in" | "sign up" }) => {
   return page === "sign in" ? (
-    <>
-      <p className="mb-2 text-xs text-slate-main600 dark:text-slate-light300">
+    <div className="mt-4 space-y-2 text-xs text-slate-main600 dark:text-slate-light300">
+      <p>
         Forgot your{" "}
-        <Link className="border-b font-semibold text-green-main500" href="/">
+        <Link
+          className="border-b text-sm font-semibold text-green-main500"
+          href="/"
+        >
           password?
         </Link>
       </p>
-      <p className="text-xs text-slate-main600 dark:text-slate-light300">
+      <p>
         New to Noteset?{" "}
         <Link
-          className="border-b font-semibold text-green-main500"
+          className="border-b text-sm font-semibold text-green-main500"
           href="/sign-up"
         >
           Sign up here.
         </Link>
       </p>
-    </>
+    </div>
   ) : (
-    <p className="mb-4 text-xs text-slate-main600 dark:text-slate-light300">
+    <p className="mt-4 text-xs text-slate-main600 dark:text-slate-light300">
       Already have an acount?{" "}
-      <Link href="/" className="border-b font-semibold text-green-main500">
+      <Link
+        href="/"
+        className="border-b text-sm font-semibold text-green-main500"
+      >
         Sign in here.
       </Link>
     </p>
