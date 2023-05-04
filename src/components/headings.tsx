64c3d79c77aca40871986e91fc0ff979e66265ea
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-04 21:28:58 +0200

    Color change for text

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -1,7 +1,7 @@
 export const SignContentHeading = ({ page }: { page: string }) => {
   return (
     <>
-      <p className="mb-3 text-lg font-bold text-slate-main600 dark:text-yellow-text50 xs:text-xl">
+      <p className="mb-3 text-lg font-bold text-slate-main600 dark:text-slate-light50 xs:text-xl">
         {page === "sign in"
           ? "Sign in, start your workout."
           : "Sign up, enjoy tracking."}
