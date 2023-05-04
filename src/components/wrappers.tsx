b71c239ea5ee979adf9202051d43b2b5135be63c
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-04 21:29:26 +0200

    Color change for text

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -12,7 +12,7 @@ export const OAuthBtnWrapper = ({ children }: WrapperProp) => {
       className="flex min-w-full items-center justify-center gap-3 
       rounded-xl border-2 border-green-main500 bg-slate-light50 
       py-2 text-sm font-semibold text-slate-main600 
-      dark:bg-slate-dark800 dark:text-yellow-text50"
+      dark:bg-slate-dark800 dark:text-slate-light50"
     >
       {children}
     </motion.button>
