3d2ff62e18813c133e61cbdae7a0b8d1e583cf2a
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-12 20:14:17 +0200

    .

--- src/components/signed-in-components/page-content.tsx
+++ src/components/signed-in-components/page-content.tsx
@@ -1,25 +1,16 @@
 import { useCardInFocus } from "../../hooks/useCardInFocus";
 import type { PageContentProps } from "../../types/types";
-import { EmptyPage } from "./empty-page";
 import { Workouts } from "./workouts";
 import { EditBar, CarouselNavigation } from "../buttons";
 
 export const PageContent = ({
   workouts,
   addWorkout,
-  changeWorkout,
-  removeWorkout,
   editBar,
   toggleEditBar,
 }: PageContentProps) => {
   const { cardInFocus, prevCard, nextCard, switchInFocus, jumpToCard } =
     useCardInFocus(workouts);
-
-  const currWorkout = workouts[cardInFocus];
-
-  if (!currWorkout) {
-    return <EmptyPage />;
-  }
 
   return (
     <>
