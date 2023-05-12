b261b6d17603f77ca0ee74dacb2038037db0ac92
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-12 20:13:18 +0200

    .

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -3,8 +3,8 @@ export const SignContentHeading = ({ page }: { page: string }) => {
     <>
       <p className="mb-3 text-lg font-bold text-slate-main600 dark:text-slate-light50 xs:text-xl">
         {page === "sign in"
-          ? "Sign in, start your workout."
-          : "Sign up, enjoy tracking."}
+          ? "Sign in, start tracking."
+          : "Sign up for free!"}
       </p>
       <p className="mb-3 text-sm italic text-slate-main600 dark:text-slate-light300 xs:text-base">
         continue with
