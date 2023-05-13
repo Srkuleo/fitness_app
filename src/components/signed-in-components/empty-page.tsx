4b5e34d6b6d8eb9f317c9300c7782f133cafdaf7
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-13 08:39:20 +0200

    Revert "wip"
    This reverts commit e20fbe96779523900d7fd1283025619ab058ec88.

--- src/components/signed-in-components/empty-page.tsx
+++ src/components/signed-in-components/empty-page.tsx
@@ -3,9 +3,9 @@ import { AddIcon, DbIcon } from "../svg";
 
 export const EmptyPage = () => {
   return (
-    <div className="flex h-full min-w-full flex-col items-center justify-center gap-4 xs:my-auto xs:min-h-[540px] xs:min-w-[370px] xs:flex-grow-0 xs:rounded-2xl xs:border-2 xs:border-dashed xs:border-orange-dark700 xs:bg-white xs:dark:border-orange-button600 xs:dark:bg-slate-dark800">
+    <div className="relative flex min-w-full flex-grow flex-col items-center justify-center gap-4 xs:my-auto xs:min-h-[540px] xs:min-w-[370px] xs:flex-grow-0 xs:rounded-2xl xs:border-2 xs:border-dashed xs:border-orange-dark700 xs:bg-white xs:dark:border-orange-button600 xs:dark:bg-slate-dark800">
       {DbIcon}
-      <p className="text-center font-medium italic text-slate-light500 dark:text-slate-light400">
+      <p className="font-medium text-center italic text-slate-light500 dark:text-slate-light400">
         You don&apos;t have any existing workout.
       </p>
 
