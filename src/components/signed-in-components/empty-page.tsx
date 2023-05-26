93e8a357121bc6c6d3f8f127d7d02c24f5459a23
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-27 00:55:18 +0200

    Small UI tweaking for better readability

--- src/components/signed-in-components/empty-page.tsx
+++ src/components/signed-in-components/empty-page.tsx
@@ -3,12 +3,12 @@ import { AddIcon, DbIcon } from "../svg";
 
 export const EmptyPage = () => {
   return (
-    <div className="my-auto flex flex-grow text-center flex-col items-center justify-center xs:min-w-[370px] md:relative md:mx-auto md:min-h-[520px] md:flex-none md:rounded-xl md:border-2 md:border-dashed md:border-orange-button500 md:bg-white dark:md:bg-slate-dark800">
+    <div className="my-auto flex flex-grow flex-col items-center justify-center text-center xs:min-w-[370px] md:relative md:mx-auto md:min-h-[520px] md:flex-none md:rounded-xl md:border-2 md:border-dashed md:border-orange-button500 md:bg-white dark:md:bg-slate-dark800">
       {DbIcon}
-      <p className="mt-4 text-lg text-slate-light500 dark:text-slate-light400">
-        Nothing to choose from :(
+      <p className="mt-4 text-lg font-bold text-slate-light400 dark:text-slate-light400">
+        Nothing to choose from
       </p>
-      <p className="text-sm italic text-slate-light500 dark:text-slate-light400">
+      <p className="text-sm font-semibold italic text-slate-light400/80 dark:text-slate-light500">
         No workouts available
       </p>
       <motion.button
