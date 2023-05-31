90b1554dd641b3a5f2113da4a439f238d4dcce50
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-31 23:56:14 +0200

    New wrapper for edit menu

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -18,3 +18,12 @@ export const OAuthBtnWrapper = ({ children }: WrapperProp) => {
     </motion.button>
   );
 };
+
+//Wrappers for signed-in page
+export const EditMenuWrapper = ({ children }: WrapperProp) => {
+  return (
+    <div className="absolute bottom-20 right-4 flex flex-row-reverse items-center gap-2 xs:right-8 sm:right-16 lg:right-24 2xl:right-28">
+      {children}
+    </div>
+  );
+};
