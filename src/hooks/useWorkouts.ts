a7afae01799a4242008e7fe37732cb6f192711c7
Srdjan <milicevic.srdjan0@gmail.com>
2023-05-31 23:57:51 +0200

    Updates to match new workout interface

--- src/hooks/useWorkouts.ts
+++ src/hooks/useWorkouts.ts
@@ -18,10 +18,47 @@ export const useWorkouts = () => {
 
   function initial() {
     setWorkouts([
-      { id: 0, title: "Upper 1", description: "Good one, could be better." },
-      { id: 1, title: "Upper 2", description: "A little bit better, imo." },
-      { id: 2, title: "Lower 1", description: "The best one so far!" },
-      { id: 3, title: "Lower 2", description: "The best one so far!" },
+      {
+        id: 0,
+        title: "Upper 1",
+        description: "Good one, could be better.",
+        mainBlock: ["weighted pullups", "db bench", "ohp"],
+        superSets: [
+          "heavy lateral/heavy cable row",
+          "lte/ez curls",
+          "hi cable row/l-t-h",
+        ],
+      },
+      {
+        id: 1,
+        title: "Upper 2",
+        description: "A little bit better, imo.",
+        mainBlock: ["weighted dips", "lat pulldown"],
+        superSets: ["one arm chest press/rdf", "spider curls/tricep kickback"],
+      },
+      {
+        id: 2,
+        title: "Lower 1",
+        description: "The best one so far!",
+        mainBlock: ["back squat", "RDL", "hip thrust"],
+        superSets: [
+          "hamstring curls/leg extensions",
+          "calves/leg raises",
+          "cable Y raises/rdf",
+        ],
+      },
+      {
+        id: 3,
+        title: "Lower 2",
+        description: "The best one so far!",
+        mainBlock: ["leg press"],
+        superSets: [
+          "lying ham curls/sissy",
+          "glute kickback/psoas raises",
+          "calves/QL crunchies",
+          "db lateral/rcf",
+        ],
+      },
     ]);
   }
 
