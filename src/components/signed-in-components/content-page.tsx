82d798952f4406a7986c1ea19686679ba0f026fe
Srdjan <milicevic.srdjan0@gmail.com>
2023-06-11 23:08:21 +0200

    UI improvements

--- src/components/signed-in-components/content-page.tsx
+++ src/components/signed-in-components/content-page.tsx
@@ -31,25 +31,29 @@ export const ContentPage = ({ workouts }: { workouts: WorkoutProps[] }) => {
               <p className="mb-8 text-slate-light500 dark:text-slate-light300/90">
                 {workout.description}
               </p>
-              <div className="mb-8 flex flex-col gap-2 text-center">
+              <div className="mb-8 flex flex-col gap-2 text-center text-slate-dark700 dark:text-slate-light100">
                 <div className="flex gap-4">
-                  <p className="flex-1 px-2 text-sm font-semibold uppercase text-slate-dark700">
+                  <p className="flex-1 px-2 text-sm font-semibold uppercase">
                     Main block
                   </p>
-                  <p className="flex-1 px-2 text-sm font-semibold uppercase text-slate-dark700">
+                  <p className="flex-1 px-2 text-sm font-semibold uppercase">
                     Supersets
                   </p>
                 </div>
 
-                <div className="flex gap-4 italic text-slate-light500">
-                  <ul className="flex flex-1 flex-col justify-center rounded-xl  bg-white/90 p-2 text-sm">
+                <div className="flex gap-4 italic text-slate-light500 dark:text-slate-light300/80">
+                  <ul className="flex flex-1 flex-col justify-center rounded-xl bg-white/90 px-2 py-4 text-sm shadow-sm dark:bg-slate-dark800 md:bg-none md:shadow-none">
                     {workout.mainBlock.map((exercise) => (
-                      <li key={exercise}>{exercise}</li>
+                      <li className="mb-2" key={exercise}>
+                        {exercise}
+                      </li>
                     ))}
                   </ul>
-                  <ul className="flex flex-1 flex-col justify-center rounded-xl bg-white/90 p-2 text-sm">
+                  <ul className="flex flex-1 flex-col justify-center rounded-xl bg-white/90 px-2 py-4 text-sm shadow-sm dark:bg-slate-dark800 md:bg-none md:shadow-none">
                     {workout.superSets.map((exercise) => (
-                      <li key={exercise}>{exercise}</li>
+                      <li className="mb-2" key={exercise}>
+                        {exercise}
+                      </li>
                     ))}
                   </ul>
                 </div>
@@ -60,11 +64,12 @@ export const ContentPage = ({ workouts }: { workouts: WorkoutProps[] }) => {
         </motion.div>
       </div>
 
-      <AnimatePresence>
-        {showEditMenu && (
-          <EditMenuBackdrop closeMenu={() => setShowEditMenu(false)} />
-        )}
-      </AnimatePresence>
+      {showEditMenu && (
+        <div
+          className="absolute inset-0 bg-black/30"
+          onClick={() => setShowEditMenu(false)}
+        />
+      )}
       <EditMenuWrapper>
         <EditMenuButton openMenu={() => setShowEditMenu(true)} />
         <AnimatePresence>{showEditMenu && <EditMenu />}</AnimatePresence>
@@ -77,20 +82,5 @@ export const ContentPage = ({ workouts }: { workouts: WorkoutProps[] }) => {
         nextCard={nextCard}
       />
     </>
-  );
-};
-
-const EditMenuBackdrop = ({ closeMenu }: { closeMenu: () => void }) => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { duration: 0.15, ease: "easeOut" },
-      }}
-      exit={{ opacity: 0, transition: { duration: 0.1, ease: "easeIn" } }}
-      className="absolute inset-0 bg-slate-dark950/40"
-      onClick={closeMenu}
-    />
   );
 };
