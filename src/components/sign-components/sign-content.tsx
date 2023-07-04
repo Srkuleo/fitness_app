794ad04f84a186f0d4e5ea325307c42901113de8
Srdjan <milicevic.srdjan0@gmail.com>
2023-07-04 23:52:51 +0200

    Increase top padding

--- src/components/sign-components/sign-content.tsx
+++ src/components/sign-components/sign-content.tsx
@@ -6,7 +6,7 @@ import LinkQuestions from "./link-questions";
 
 export const SignContent = ({ page }: { page: "sign in" | "sign up" }) => {
   return (
-    <div className="mx-auto pt-16 flex max-w-[280px] flex-col px-2 xs:max-w-[295px] xs:px-0">
+    <div className="mx-auto pt-24 flex max-w-[280px] flex-col px-2 xs:max-w-[295px] xs:px-0">
       <SignContentHeading page={page} />
       <OAuth />
       <Separator />
