93fd722d7b956738cfa293e2345aec4eae4e831f
Srdjan <milicevic.srdjan0@gmail.com>
2023-07-04 23:58:05 +0200

    Removes options menu variable for framer motion

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -13,29 +13,3 @@ export const initWorkout: WorkoutProps = {
   mainBlock: [],
   superSets: [],
 };
-
-//Variant for framer motion animations
-export const optionsMenuVariant = {
-  hidden: {
-    opacity: 0,
-  },
-  "fade-in": {
-    opacity: 1,
-    transition: { duration: 0.1, ease: "easeOut" },
-  },
-  "fade-out": {
-    opacity: 0,
-    transition: { delay: 0.1, duration: 0.15, ease: "easeIn" },
-  },
-
-  "most-right": { x: "100%" },
-  "slide-in": {
-    x: 0,
-    transition: { duration: 0.2, ease: "easeOut" },
-  },
-  "slide-out": {
-    x: "100%",
-    opacity: 0,
-    transition: { delay: 0.1, duration: 0.15, ease: "easeIn" },
-  },
-};
