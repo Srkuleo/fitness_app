ae0c20d978cca864c61dc7c9ae0f27067909fbc9
Srdjan <milicevic.srdjan0@gmail.com>
2023-08-30 23:59:10 +0200

    Huge refactoring of content page, still WIP

--- src/components/signed-in-components/content-page.tsx
+++ src/components/signed-in-components/content-page.tsx
@@ -1,29 +1,93 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
 import { useCardInFocus } from "../../hooks/useCardInFocus";
 import type { WorkoutProps } from "../../types/types";
-import { CarouselNav, EditMenu, WorkoutButtons } from "../buttons";
+import { CarouselNav } from "../buttons";
+import { AddIcon, CancelIcon, EditIcon, PreviewIcon, RemoveIcon } from "../svg";
+// import { useState } from "react";
 
-export const ContentPage = ({ workouts }: { workouts: WorkoutProps[] }) => {
-  const { cardInFocus, prevCard, nextCard, jumpToCard, switchInFocus } =
+export const ContentPage = ({
+  workouts,
+  showEditBar,
+  closeEditBar,
+}: {
+  workouts: WorkoutProps[];
+  showEditBar: boolean;
+  closeEditBar: () => void;
+}) => {
+  const { cardInFocus, prevCard, nextCard, switchInFocus } =
     useCardInFocus(workouts);
-  const [showEditMenu, setShowEditMenu] = useState(false);
-  const [showForm, setShowForm] = useState(false);
+  // const [showForm, setShowForm] = useState(false);
 
   return (
     <>
-      <div className="m-auto max-w-full overflow-x-hidden px-2 xs:max-w-sm md:rounded-xl md:border-2 md:border-orange-button500 md:bg-white md:px-0 md:dark:bg-slate-dark800">
+      <AnimatePresence>
+        {showEditBar && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+              y: 72,
+              transition: { duration: 0.25, ease: "easeOut" },
+            }}
+            exit={{
+              opacity: 0,
+              transition: { delay: 0.3, duration: 0.15, ease: "easeIn" },
+            }}
+            className="absolute z-10 flex gap-6"
+          >
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className="rounded-xl bg-white p-2 shadow-md dark:shadow-slate-dark900 "
+            >
+              <AddIcon
+                className="h-5 w-5 text-slate-main600"
+                strokeWidth={1.8}
+              />
+            </motion.button>
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className="rounded-xl bg-white p-2 shadow-md dark:shadow-slate-dark900 "
+            >
+              <EditIcon
+                className="h-5 w-5 text-green-dark600 dark:text-slate-main600"
+                strokeWidth={1.8}
+              />
+            </motion.button>
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className="rounded-xl bg-white p-2 shadow-md dark:shadow-slate-dark900 "
+            >
+              <RemoveIcon
+                className="h-5 w-5 text-red-removeBtn600"
+                strokeWidth={1.8}
+              />
+            </motion.button>
+            <motion.button
+              onClick={closeEditBar}
+              whileTap={{ scale: 0.9 }}
+              className="rounded-xl bg-white p-2 shadow-md dark:shadow-slate-dark900 "
+            >
+              <CancelIcon
+                className="h-5 w-5 text-slate-main600"
+                strokeWidth={1.8}
+              />
+            </motion.button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <div className="flex max-w-full flex-1 overflow-x-hidden px-2 xs:max-w-sm md:m-auto md:max-h-[520px] md:rounded-xl md:border-2 md:border-orange-button500 md:bg-white md:px-0 md:dark:bg-slate-dark800">
         <motion.div
           animate={{
             transition: { duration: 0.4, ease: "easeOut" },
             transform: `translateX(-${cardInFocus * 100}%)`,
           }}
-          className="flex items-center"
+          className="flex w-full"
         >
           {workouts.map((workout) => (
             <div
               key={workout.id}
-              className="flex min-w-full flex-col items-center py-4 md:py-8"
+              className="flex h-full min-w-full flex-col items-center justify-center py-4 md:py-8"
             >
               <p className="mb-2 text-2xl font-semibold text-slate-main600 dark:text-slate-light200">
                 {workout.title}
@@ -31,36 +95,25 @@ export const ContentPage = ({ workouts }: { workouts: WorkoutProps[] }) => {
               <p className="mb-8 text-slate-light500 dark:text-slate-light300/90">
                 {workout.description}
               </p>
-              <WorkoutButtons />
+              <div className="flex gap-2">
+                <motion.button
+                  whileTap={{ scale: 0.9 }}
+                  className="rounded-xl bg-white px-3 py-2 text-slate-main600 shadow-md dark:shadow-slate-dark900"
+                >
+                  {PreviewIcon}
+                </motion.button>
+                <motion.button
+                  whileTap={{ scale: 0.9 }}
+                  className="rounded-xl bg-gradient-to-r from-orange-button500
+      to-red-button500 px-6 py-2 font-semibold uppercase text-slate-light200 shadow-md dark:shadow-slate-dark900"
+                >
+                  Start
+                </motion.button>
+              </div>
             </div>
           ))}
         </motion.div>
       </div>
-
-      <AnimatePresence>
-        {showEditMenu && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-              transition: { duration: 0.15, ease: "easeOut" },
-            }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.1, ease: "easeIn" },
-            }}
-            className="absolute inset-0 bg-black/30 md:z-10"
-            onClick={() => setShowEditMenu(false)}
-          />
-        )}
-      </AnimatePresence>
-
-      <EditMenu
-        openMenu={() => setShowEditMenu(true)}
-        showEditMenu={showEditMenu}
-        hideEditMenu={() => setShowEditMenu(false)}
-        showForm={() => setShowForm(true)}
-      />
 
       <CarouselNav
         workouts={workouts}
