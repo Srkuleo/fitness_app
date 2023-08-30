e75b0bf50d74e571a563835303319b0172ee6f39
Srdjan <milicevic.srdjan0@gmail.com>
2023-08-31 00:01:28 +0200

    NavBar returns

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { NavBar } from "../components/sign-components/nav-bar";
 import { SignContent } from "../components/sign-components/sign-content";
 
 const SignUpPage: NextPage = () => {
@@ -13,7 +14,7 @@ const SignUpPage: NextPage = () => {
           content="Personalized workout tracker - sign up"
         />
       </Head>
-
+      <NavBar />
       <SignContent page="sign up" />
     </>
   );
