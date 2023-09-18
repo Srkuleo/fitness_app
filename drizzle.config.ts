df1c1c2784fa9b160a35a70564cbda2bc1fbacce
Srdjan <milicevic.srdjan0@gmail.com>
2023-09-18 23:50:54 +0200

    Configurations for drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
 import type { Config } from "drizzle-kit";
-
-import "dotenv/config";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable not provided.");
-}
+import { env } from "./src/env.mjs";
 
 export default {
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: process.env.
+    connectionString: env.DATABASE_URL,
   },
 } satisfies Config;
