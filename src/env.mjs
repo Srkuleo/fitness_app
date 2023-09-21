31af0f9604c1d91584379b2421eef6ac3f131cdf
Srdjan <milicevic.srdjan0@gmail.com>
2023-09-21 12:53:39 +0200

    Fixes reading env file bug

--- src/env.mjs
+++ src/env.mjs
@@ -1,6 +1,9 @@
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
 import { createEnv } from "@t3-oss/env-nextjs";
-
 import { z } from "zod";
+
+loadEnvConfig(cwd());
 
 export const env = createEnv({
   server: {
